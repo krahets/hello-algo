b47f54886e609c17518053487272ac6d17ed4c0f
pengchzn <pengchzn@gmail.com>
2022-11-29 13:58:23 +0800

    栈的python代码

--- codes/python/chapter_stack_and_queue/array_stack.py
+++ codes/python/chapter_stack_and_queue/array_stack.py
@@ -4,7 +4,78 @@ Created Time: 2022-11-25
 Author: Krahets (krahets@163.com)
 '''
 
-import sys, os.path as osp
+import os.path as osp
+import sys
+
 sys.path.append(osp.dirname(osp.dirname(osp.abspath(__file__))))
 from include import *
 
+""" 基于数组实现的栈 """
+
+
+class ArrayStack:
+    def __init__(self):
+        self._stack = []
+        self._size = 0
+
+    """ 获取栈的长度 """
+
+    def size(self):
+        return self._size
+
+    """ 判断栈是否为空 """
+
+    def isEmpty(self):
+        return self._stack == []
+
+    """ 入栈 """
+
+    def push(self, item):
+        self._stack.append(item)
+        self._size += 1
+
+    """ 出栈 """
+
+    def pop(self):
+        pop = self._stack.pop()
+        self._size -= 1
+        return pop
+
+    """ 访问栈顶元素 """
+
+    def peek(self):
+        return self._stack[-1]
+
+    """ 访问索引 index 处元素 """
+
+    def get(self, index):
+        return self._stack[index]
+
+
+if __name__ == "__main__":
+    """ 初始化栈 """
+    stack = ArrayStack()
+
+    """ 元素入栈 """
+    stack.push(1)
+    stack.push(3)
+    stack.push(2)
+    stack.push(5)
+    stack.push(4)
+    print("栈 stack = ", stack._stack)
+
+    """ 访问栈顶元素 """
+    peek = stack.peek()
+    print("栈顶元素 peek = ", peek)
+
+    """ 元素出栈 """
+    pop = stack.pop()
+    print("出栈元素 pop = ", pop)
+    print("出栈后 stack = ", stack._stack)
+
+    """ 获取栈的长度 """
+    size = stack.size()
+    print("栈的长度 size = ", size)
+
+    """ 判断是否为空 """
+    isEmpty = (stack == [])
--- codes/python/chapter_stack_and_queue/stack.py
+++ codes/python/chapter_stack_and_queue/stack.py
@@ -4,7 +4,36 @@ Created Time: 2022-11-25
 Author: Krahets (krahets@163.com)
 '''
 
-import sys, os.path as osp
+import os.path as osp
+import sys
+
 sys.path.append(osp.dirname(osp.dirname(osp.abspath(__file__))))
 from include import *
 
+if __name__ == "__main__":
+    """ 初始化栈 """
+    stack = []
+
+    """ 元素入栈 """
+    stack.append(1)
+    stack.append(3)
+    stack.append(2)
+    stack.append(5)
+    stack.append(4)
+    print("栈 stack = ", stack)
+
+    """ 访问栈顶元素 """
+    peek = stack[-1]
+    print("栈顶元素 peek = ", peek)
+
+    """ 元素出栈 """
+    pop = stack.pop()
+    print("出栈元素 pop = ", pop)
+    print("出栈后 stack = ", stack)
+
+    """ 获取栈的长度 """
+    size = len(stack)
+    print("栈的长度 size = ", size)
+
+    """ 判断是否为空 """
+    isEmpty = (stack == [])
