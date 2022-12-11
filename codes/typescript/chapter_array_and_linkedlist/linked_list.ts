41dbb8a05455bb075ed76915e86bb2e72b0783e9
Yudong Jin <krahets@163.com>
2022-12-12 02:24:14 +0800

    Recover the TS code.

---
+++ codes/typescript/chapter_array_and_linkedlist/linked_list.ts
@@ -0,0 +1,82 @@
+/*
+ * File: linked_list.ts
+ * Created Time: 2022-12-10
+ * Author: Justin (xiefahit@gmail.com)
+ */
+
+import ListNode from '../module/ListNode';
+import { printLinkedList } from '../module/PrintUtil';
+
+function insert(n0: ListNode, P: ListNode): void {
+    const n1 = n0.next;
+    n0.next = P;
+    P.next = n1;
+}
+
+function remove(n0: ListNode): void {
+    if (!n0.next) {
+        return;
+    }
+    // n0 -> P -> n1
+    const P = n0.next;
+    const n1 = P.next;
+    n0.next = n1;
+}
+
+function access(head: ListNode | null, index: number): ListNode | null {
+    for (let i = 0; i < index; i++) {
+        if (!head) {
+            return null;
+        }
+        head = head.next;
+    }
+    return head;
+}
+
+function find(head: ListNode | null, target: number): number {
+    let index = 0;
+    while (head !== null) {
+        if (head.val === target) {
+            return index;
+        }
+        head = head.next;
+        index += 1;
+    }
+    return -1;
+}
+
+/* Driver Code */
+/* 初始化链表 */
+// 初始化各个结点
+const n0 = new ListNode(1);
+const n1 = new ListNode(3);
+const n2 = new ListNode(2);
+const n3 = new ListNode(5);
+const n4 = new ListNode(4);
+// 构建引用指向
+n0.next = n1;
+n1.next = n2;
+n2.next = n3;
+n3.next = n4;
+console.log('初始化的链表为');
+printLinkedList(n0);
+
+/* 插入结点 */
+insert(n0, new ListNode(0));
+console.log('插入结点后的链表为');
+printLinkedList(n0);
+
+/* 删除结点 */
+remove(n0);
+console.log('删除结点后的链表为');
+printLinkedList(n0);
+
+/* 访问结点 */
+const node = access(n0, 3);
+console.log(`链表中索引 3 处的结点的值 = ${node?.val}`);
+
+/* 查找结点 */
+const index = find(n0, 2);
+console.log(`链表中值为 2 的结点的索引 = ${index}`);
+
+export {};
---
+++ codes/typescript/module/ListNode.ts
@@ -0,0 +1,9 @@
+// Definition for singly-linked list.
+export default class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
