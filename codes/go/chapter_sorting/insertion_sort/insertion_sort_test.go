d612bea74859a01778e8ba9cbbfd1d1c001506cc
Yudong Jin <krahets@163.com>
2022-12-12 20:03:34 +0800

    Fix the Go code of insertion sort.

--- codes/go/chapter_sorting/insertion_sort/insertion_sort_test.go
+++ codes/go/chapter_sorting/insertion_sort/insertion_sort_test.go
@@ -1,6 +1,6 @@
-//File: insertion_sort_test.go
-//Created Time: 2022-12-12
-//Author: msk397 (machangxinq@gmail.com)
+// File: insertion_sort_test.go
+// Created Time: 2022-12-12
+// Author: msk397 (machangxinq@gmail.com)
 
 package insertion_sort
 
@@ -12,5 +12,5 @@ import (
 func TestInsertionSort(t *testing.T) {
 	nums := []int{4, 1, 3, 1, 5, 2}
 	insertionSort(nums)
-	fmt.Println("插入排序完成后 nums = ", nums)
+	fmt.Println("插入排序完成后 nums =", nums)
 }
