2305b09db4935fb123fffc5a77f6c1663566fac4
machangxin <machangxinq@gmail.com>
2022-12-17 15:14:38 +0800

    Supplement linear_search documentation

--- codes/go/chapter_searching/linear_search.go
+++ codes/go/chapter_searching/linear_search.go
@@ -8,12 +8,12 @@ import (
 	. "github.com/krahets/hello-algo/pkg"
 )
 
-// linerSearchArray 线性查找（数组）
+/* 线性查找（数组） */
 func linerSearchArray(nums []int, target int) int {
 	// 遍历数组
 	for i := 0; i < len(nums); i++ {
+		// 找到目标元素，返回其索引
 		if nums[i] == target {
-			// 找到目标元素，返回其索引
 			return i
 		}
 	}
@@ -21,10 +21,11 @@ func linerSearchArray(nums []int, target int) int {
 	return -1
 }
 
-// linerSearchLinkedList 线性查找（链表）
+/* 线性查找（链表） */
 func linerSearchLinkedList(node *ListNode, target int) *ListNode {
 	// 遍历链表
 	for node != nil {
+		// 找到目标元素，返回其索引
 		if node.Val == target {
 			return node
 		}
