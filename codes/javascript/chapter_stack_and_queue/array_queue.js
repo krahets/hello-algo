2f5643dc903fcde9e4b35d58aef279367f940be4
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-12-19 23:46:53 +0800

    Revert "fix: use private variables"
    This reverts commit 09f9c81eb2df5e3577cdf3c1b31de37d963e0e93.

--- codes/javascript/chapter_stack_and_queue/array_queue.js
+++ codes/javascript/chapter_stack_and_queue/array_queue.js
@@ -7,29 +7,29 @@
 
 /* 基于环形数组实现的队列 */
 class ArrayQueue {
-    #queue;     // 用于存储队列元素的数组
-    #front = 0;  // 头指针，指向队首
-    #rear = 0;   // 尾指针，指向队尾 + 1
-    #CAPACITY = 1e5;
+    queue;     // 用于存储队列元素的数组
+    front = 0;  // 头指针，指向队首
+    rear = 0;   // 尾指针，指向队尾 + 1
+    CAPACITY = 1e5;
 
     constructor(capacity) {
-        this.#queue = new Array(capacity ?? this.CAPACITY);
+        this.queue = new Array(capacity ?? this.CAPACITY);
     }
 
     /* 获取队列的容量 */
     get capacity() {
-        return this.#queue.length;
+        return this.queue.length;
     }
 
     /* 获取队列的长度 */
     get size() {
         // 由于将数组看作为环形，可能 rear < front ，因此需要取余数
-        return (this.capacity + this.#rear - this.#front) % this.capacity;
+        return (this.capacity + this.rear - this.front) % this.capacity;
     }
 
     /* 判断队列是否为空 */
     empty() {
-        return this.#rear - this.#front == 0;
+        return this.rear - this.front == 0;
     }
 
     /* 入队 */
@@ -39,16 +39,16 @@ class ArrayQueue {
             return;
         }
         // 尾结点后添加 num
-        this.#queue[this.#rear] = num;
+        this.queue[this.rear] = num;
         // 尾指针向后移动一位，越过尾部后返回到数组头部
-        this.#rear = (this.#rear + 1) % this.capacity;
+        this.rear = (this.rear + 1) % this.capacity;
     }
 
     /* 出队 */
     poll() {
         const num = this.peek();
         // 队头指针向后移动一位，若越过尾部则返回到数组头部
-        this.#front = (this.#front + 1) % this.capacity;
+        this.front = (this.front + 1) % this.capacity;
         return num;
     }
 
@@ -57,14 +57,14 @@ class ArrayQueue {
         // 删除头结点
         if (this.empty())
             throw new Error("The queue is empty!");
-        return this.#queue[this.#front];
+        return this.queue[this.front];
     }
 
     /* 访问指定索引元素 */
     get(index) {
         if (index >= this.size)
             throw new Error("Index out of bounds!");
-        return this.#queue[(this.#front + index) % this.capacity];
+        return this.queue[(this.front + index) % this.capacity];
     }
 
     /* 返回 Array */
@@ -73,8 +73,8 @@ class ArrayQueue {
         const cap = this.capacity;
         // 仅转换有效长度范围内的列表元素
         const arr = new Array(siz);
-        for (let i = 0, j = this.#front; i < siz; i++, j++) {
-            arr[i] = this.#queue[j % cap];
+        for (let i = 0, j = this.front; i < siz; i++, j++) {
+            arr[i] = this.queue[j % cap];
         }
         return arr;
     }
