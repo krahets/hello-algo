63cd3e4f65117235461437df3f02604cf93aadc5
RiverTwilight <wrj2014@126.com>
2023-01-02 21:16:24 +0800

    lint: added prettier config and switch indent type

--- codes/javascript/chapter_computational_complexity/time_complexity.js
+++ codes/javascript/chapter_computational_complexity/time_complexity.js
@@ -6,116 +6,116 @@
 
 /* 常数阶 */
 function constant(n) {
-	let count = 0;
-	const size = 100000;
-	for (let i = 0; i < size; i++) count++;
-	return count;
+    let count = 0;
+    const size = 100000;
+    for (let i = 0; i < size; i++) count++;
+    return count;
 }
 
 /* 线性阶 */
 function linear(n) {
-	let count = 0;
-	for (let i = 0; i < n; i++) count++;
-	return count;
+    let count = 0;
+    for (let i = 0; i < n; i++) count++;
+    return count;
 }
 
 /* 线性阶（遍历数组） */
 function arrayTraversal(nums) {
-	let count = 0;
-	// 循环次数与数组长度成正比
-	for (let i = 0; i < nums.length; i++) {
-		count++;
-	}
-	return count;
+    let count = 0;
+    // 循环次数与数组长度成正比
+    for (let i = 0; i < nums.length; i++) {
+        count++;
+    }
+    return count;
 }
 
 /* 平方阶 */
 function quadratic(n) {
-	let count = 0;
-	// 循环次数与数组长度成平方关系
-	for (let i = 0; i < n; i++) {
-		for (let j = 0; j < n; j++) {
-			count++;
-		}
-	}
-	return count;
+    let count = 0;
+    // 循环次数与数组长度成平方关系
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            count++;
+        }
+    }
+    return count;
 }
 
 /* 平方阶（冒泡排序） */
 function bubbleSort(nums) {
-	let count = 0; // 计数器
-	// 外循环：待排序元素数量为 n-1, n-2, ..., 1
-	for (let i = nums.length - 1; i > 0; i--) {
-		// 内循环：冒泡操作
-		for (let j = 0; j < i; j++) {
-			if (nums[j] > nums[j + 1]) {
-				// 交换 nums[j] 与 nums[j + 1]
-				let tmp = nums[j];
-				nums[j] = nums[j + 1];
-				nums[j + 1] = tmp;
-				count += 3; // 元素交换包含 3 个单元操作
-			}
-		}
-	}
-	return count;
+    let count = 0; // 计数器
+    // 外循环：待排序元素数量为 n-1, n-2, ..., 1
+    for (let i = nums.length - 1; i > 0; i--) {
+        // 内循环：冒泡操作
+        for (let j = 0; j < i; j++) {
+            if (nums[j] > nums[j + 1]) {
+                // 交换 nums[j] 与 nums[j + 1]
+                let tmp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = tmp;
+                count += 3; // 元素交换包含 3 个单元操作
+            }
+        }
+    }
+    return count;
 }
 
 /* 指数阶（循环实现） */
 function exponential(n) {
-	let count = 0,
-		base = 1;
-	// cell 每轮一分为二，形成数列 1, 2, 4, 8, ..., 2^(n-1)
-	for (let i = 0; i < n; i++) {
-		for (let j = 0; j < base; j++) {
-			count++;
-		}
-		base *= 2;
-	}
-	// count = 1 + 2 + 4 + 8 + .. + 2^(n-1) = 2^n - 1
-	return count;
+    let count = 0,
+        base = 1;
+    // cell 每轮一分为二，形成数列 1, 2, 4, 8, ..., 2^(n-1)
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < base; j++) {
+            count++;
+        }
+        base *= 2;
+    }
+    // count = 1 + 2 + 4 + 8 + .. + 2^(n-1) = 2^n - 1
+    return count;
 }
 
 /* 指数阶（递归实现） */
 function expRecur(n) {
-	if (n == 1) return 1;
-	return expRecur(n - 1) + expRecur(n - 1) + 1;
+    if (n == 1) return 1;
+    return expRecur(n - 1) + expRecur(n - 1) + 1;
 }
 
 /* 对数阶（循环实现） */
 function logarithmic(n) {
-	let count = 0;
-	while (n > 1) {
-		n = n / 2;
-		count++;
-	}
-	return count;
+    let count = 0;
+    while (n > 1) {
+        n = n / 2;
+        count++;
+    }
+    return count;
 }
 
 /* 对数阶（递归实现） */
 function logRecur(n) {
-	if (n <= 1) return 0;
-	return logRecur(n / 2) + 1;
+    if (n <= 1) return 0;
+    return logRecur(n / 2) + 1;
 }
 
 /* 线性对数阶 */
 function linearLogRecur(n) {
-	if (n <= 1) return 1;
-	let count = linearLogRecur(n / 2) + linearLogRecur(n / 2);
-	for (let i = 0; i < n; i++) {
-		count++;
-	}
-	return count;
+    if (n <= 1) return 1;
+    let count = linearLogRecur(n / 2) + linearLogRecur(n / 2);
+    for (let i = 0; i < n; i++) {
+        count++;
+    }
+    return count;
 }
 
 /* 阶乘阶（递归实现） */
 function factorialRecur(n) {
-	if (n == 0) return 1;
-	let count = 0;
-	// 从 1 个分裂出 n 个
-	for (let i = 0; i < n; i++) {
-		count += factorialRecur(n - 1);
-	}
-	return count;
+    if (n == 0) return 1;
+    let count = 0;
+    // 从 1 个分裂出 n 个
+    for (let i = 0; i < n; i++) {
+        count += factorialRecur(n - 1);
+    }
+    return count;
 }
 
 const n = 8;
