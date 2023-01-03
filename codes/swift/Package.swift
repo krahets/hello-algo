2069dddd3b1c8a5271d269f853c1e3f44e153962
nuomi1 <nuomi1@qq.com>
2023-01-03 22:24:50 +0800

    feat: add Swift codes for space time tradeoff article

--- codes/swift/Package.swift
+++ codes/swift/Package.swift
@@ -8,11 +8,13 @@ let package = Package(
         .executable(name: "time_complexity", targets: ["time_complexity"]),
         .executable(name: "worst_best_time_complexity", targets: ["worst_best_time_complexity"]),
         .executable(name: "space_complexity", targets: ["space_complexity"]),
+        .executable(name: "leetcode_two_sum", targets: ["leetcode_two_sum"]),
     ],
     targets: [
         .target(name: "utils", path: "utils"),
         .executableTarget(name: "time_complexity", path: "chapter_computational_complexity", sources: ["time_complexity.swift"]),
         .executableTarget(name: "worst_best_time_complexity", path: "chapter_computational_complexity", sources: ["worst_best_time_complexity.swift"]),
         .executableTarget(name: "space_complexity", dependencies: ["utils"], path: "chapter_computational_complexity", sources: ["space_complexity.swift"]),
+        .executableTarget(name: "leetcode_two_sum", path: "chapter_computational_complexity", sources: ["leetcode_two_sum.swift"]),
     ]
 )
