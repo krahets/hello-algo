33023549095ae85eab68ab1c0e580a4ef14ad002
Yudong Jin <krahets@163.com>
2023-01-05 01:45:08 +0800

    Update cpp codes for the stack and the queue.

--- codes/cpp/chapter_stack_and_queue/array_stack.cpp
+++ codes/cpp/chapter_stack_and_queue/array_stack.cpp
@@ -28,10 +28,9 @@ public:
     }
 
     /* 出栈 */
-    int pop() {
+    void pop() {
         int oldTop = top();
         stack.pop_back();
-        return oldTop;
     }
 
     /* 访问栈顶元素 */
@@ -67,8 +66,8 @@ int main() {
     cout << "栈顶元素 top = " << top << endl;
 
     /* 元素出栈 */
-    int pop = stack->pop();
-    cout << "出栈元素 pop = " << pop << "，出栈后 stack = ";
+    stack->pop();
+    cout << "出栈元素 pop = " << top << "，出栈后 stack = ";
     PrintUtil::printVector(stack->toVector());
 
     /* 获取栈的长度 */
