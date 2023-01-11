40b5be53021d459ad22263b4dcc8e56aff5784fa
Yudong Jin <krahets@163.com>
2023-01-12 00:31:11 +0800

    Unify the naming of the C codes.

--- codes/c/chapter_computational_complexity/worst_best_time_complexity.c
+++ codes/c/chapter_computational_complexity/worst_best_time_complexity.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
         int *nums = randomNumbers(n);
         int index = findOne(nums, n);
         printf("\n数组 [ 1, 2, ..., n ] 被打乱后 = ");
-        PrintArray(nums, n);
+        printArray(nums, n);
         printf("数字 1 的索引为 %d\n", index);
         // 释放堆区内存
         if (nums != NULL) {
