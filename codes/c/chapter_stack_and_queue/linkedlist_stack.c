153846c94b3008e67c46d9ff1302f8a2fe9509ab
Gonglja <glj0@outlook.com>
2023-01-12 21:09:51 +0800

    feat(codes/c/): add array_stack and linkedlist_stack frame

---
+++ codes/c/chapter_stack_and_queue/linkedlist_stack.c
@@ -0,0 +1,42 @@
+/**
+ * File: linkedlist_stack.c
+ * Created Time: 2022-01-12
+ * Author: Zero (glj0@outlook.com)
+ */
+
+#include "../include/include.h"
+
+struct LinkedListStack {
+    ListNode* stkTop;
+    size_t stkSize;
+};
+
+typedef struct LinkedListStack LinkedListStack;
+
+void new(LinkedListStack* stk) {
+
+}
+
+size_t size(LinkedListStack* stk) {
+
+}
+
+bool empty(LinkedListStack* stk) {
+
+}
+
+void push(LinkedListStack* stk, int num) {
+
+}
+
+void pop(LinkedListStack* stk) {
+
+}
+
+int top(LinkedListStack* stk) {
+
+}
+
+int main() {
+    return 0;
+}
