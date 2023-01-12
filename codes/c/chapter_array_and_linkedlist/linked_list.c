be2d109c5be6ca48d50f233ac4200858f98a881f
Gonglja <glj0@outlook.com>
2023-01-12 15:16:57 +0800

    style(codes/c): update comment format

--- codes/c/chapter_array_and_linkedlist/linked_list.c
+++ codes/c/chapter_array_and_linkedlist/linked_list.c
@@ -4,47 +4,44 @@
  * Author: Zero (glj0@outlook.com)
  */
 
- #include "../include/include.h"
+#include "../include/include.h"
 
  /* 在链表的结点 n0 之后插入结点 P */
 void insertNode(ListNode* n0, ListNode* P) {
-    ListNode *n1;
-    n1 = n0 -> next;
-    n0 -> next = P;
-    P -> next = n1;
+    ListNode *n1 = n0->next;
+    n0->next = P;
+    P->next = n1;
 }
 
 /* 删除链表的结点 n0 之后的首个结点 */
 void removeNode(ListNode* n0) {
+    if (!n0->next)
+        return;
     // n0 -> P -> n1
-    ListNode *n1, *P;
-    P = n0 -> next;
-    n1 = P -> next;
-    n0 -> next = n1;
-
+    ListNode *P = n0->next;
+    ListNode *n1 = P->next;
+    n0->next = n1;
+    // 释放内存
     free(P);
 }
 
 /* 访问链表中索引为 index 的结点 */
 ListNode* accessNode(ListNode* head, int index) {
-    ListNode* n;
-    while(head && head->next && index) {
+    while (head && head->next && index) {
         head = head->next;
-        index --;
+        index--;
     }
-    n = head;
+    return head;
 }
 
 /* 在链表中查找值为 target 的首个结点 */
 int findNode(ListNode* head, int target) {
-    ListNode* n;
-    int idx=0;
-    while(head) {
-        if(head->val == target) {
-            return idx;
-        }
-        head = head -> next;
-        idx ++;
+    int index = 0;
+    while (head) {
+        if (head->val == target)
+            return index;
+        head = head->next;
+        index++;
     }
     return -1;
 }
@@ -55,10 +52,10 @@ int main() {
     /* 初始化链表 */
     // 初始化各个结点 
     ListNode* n0 = newListNode(1);
-    ListNode* n1 = newListNode(2);
-    ListNode* n2 = newListNode(3);
-    ListNode* n3 = newListNode(4);
-    ListNode* n4 = newListNode(5);
+    ListNode* n1 = newListNode(3);
+    ListNode* n2 = newListNode(2);
+    ListNode* n3 = newListNode(5);
+    ListNode* n4 = newListNode(4);
     // 构建引用指向
     n0->next = n1;
     n1->next = n2;
@@ -82,8 +79,8 @@ int main() {
     printf("链表中索引 3 处的结点的值 = %d\r\n", node->val);
 
     /* 查找结点 */
-    int index = findNode(n0, 5);
-    printf("链表中值为 5 的结点的索引 = %d\r\n", index);
+    int index = findNode(n0, 2);
+    printf("链表中值为 2 的结点的索引 = %d\r\n", index);
 
     return 0;
 }
