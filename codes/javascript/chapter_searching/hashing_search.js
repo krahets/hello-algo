6ad8a66a7c44a2c415d803153277161b711c8969
Justin Tse <xief008@outlook.com>
2023-02-05 15:40:30 +0800

    Update the coding style for JavaScript (#329)
    * Fix bug before commit 5eae708
    
    * Update queue.md
    
    * Update the coding style for JavaScript
    
    ---------
    
    Co-authored-by: Yudong Jin <krahets@163.com>

--- codes/javascript/chapter_searching/hashing_search.js
+++ codes/javascript/chapter_searching/hashing_search.js
@@ -4,9 +4,7 @@
  * Author: Zhuo Qinyue (1403450829@qq.com)
  */
 
-const PrintUtil = require("../include/PrintUtil");
-const ListNode = require("../include/ListNode");
-
+const { ListNode, arrToLinkedList } = require("../include/ListNode");
 
 /* 哈希查找（数组） */
 function hashingSearchArray(map, target) {
@@ -22,30 +20,26 @@ function hashingSearchLinkedList(map, target) {
     return map.has(target) ? map.get(target) : null;
 }
 
-function main() {
-    const target = 3;
+/* Driver Code */
+const target = 3;
 
-    /* 哈希查找（数组） */
-    const nums = [1, 5, 3, 2, 4, 7, 5, 9, 10, 8];
-    // 初始化哈希表
-    const map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-      map.set(nums[i], i);  // key: 元素，value: 索引
-    }
-    const index = hashingSearchArray(map, target);
-    console.log("目标元素 3 的索引 = " + index);
-
-    /* 哈希查找（链表） */
-    let head = new ListNode().arrToLinkedList(nums)
-    // 初始化哈希表
-    const map1 = new Map();
-    while (head != null) {
-      map1.set(head.val, head);  // key: 结点值，value: 结点
-      head = head.next;
-    }
-    const node = hashingSearchLinkedList(map1, target);
-    console.log("目标结点值 3 的对应结点对象为" );
-    PrintUtil.printLinkedList(node);
+/* 哈希查找（数组） */
+const nums = [1, 5, 3, 2, 4, 7, 5, 9, 10, 8];
+// 初始化哈希表
+const map = new Map();
+for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], i);  // key: 元素，value: 索引
 }
+const index = hashingSearch(map, target);
+console.log("目标元素 3 的索引 = " + index);
 
-main();
+/* 哈希查找（链表） */
+let head = arrToLinkedList(nums)
+// 初始化哈希表
+const map1 = new Map();
+while (head != null) {
+    map1.set(head.val, head);  // key: 结点值，value: 结点
+    head = head.next;
+}
+const node = hashingSearch1(map1, target);
+console.log("目标结点值 3 的对应结点对象为", node);
