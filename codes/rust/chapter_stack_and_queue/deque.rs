856507c5e7743267ef9abd2eb31ef591f4836a94
xblackicex <xblackicex@outlook.com>
2023-02-14 19:51:21 +0100

    📃 docs(deque): corrent author format

--- codes/rust/chapter_stack_and_queue/deque.rs
+++ codes/rust/chapter_stack_and_queue/deque.rs
@@ -1,7 +1,7 @@
 /*
  * File: deque.rs
  * Created Time: 2023-02-05
- * Author: sjinzh (sjinzh@gmail.com), (xBLACKICEx@outlook.com)
+ * Author: sjinzh (sjinzh@gmail.com), xBLACKICEx (xBLACKICEx@outlook.com)
  */
 
 include!("../include/include.rs");
