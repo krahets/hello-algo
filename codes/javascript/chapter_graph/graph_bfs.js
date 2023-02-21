042437e2fa375a562d4174bce3753dc6c27ffe9a
steak-zhuo <zhuoqinyue@gmail.com>
2023-02-21 08:05:23 -0600

    add bfs and dfs for JS and TS

---
+++ codes/javascript/chapter_graph/graph_bfs.js
@@ -0,0 +1,52 @@
+/**
+ * File: graph_bfs.js
+ * Created Time: 2023-02-21
+ * Author: Zhuo Qinyue (1403450829@qq.com)
+ */
+
+
+const { GraphAdjList } = require('./graph_adjacency_list');
+const { Vertex } = require('../include/Vertex');
+
+
+/* 广度优先遍历 BFS */
+// 使用邻接表来表示图，以便获取指定顶点的所有邻接顶点
+const graphBfs = (graph, startVet) => {
+    // 顶点遍历序列
+    const res = [];
+    // 哈希表，用于记录已被访问过的顶点
+    const visited = new Set();
+    visited.add(startVet);
+    // 队列用于实现 BFS
+    const que = [startVet];
+
+    // 以顶点 vet 为起点，循环直至访问完所有顶点
+    while (que.length) {
+        const vet = que.shift();    // 队首顶点出队
+        res.push(vet);              // 记录访问顶点
+        // 遍历该顶点的所有邻接顶点
+        for (const adjVet of graph.adjList.get(vet) ?? []) {
+            if (visited.has(adjVet)) {
+                continue;           // 跳过已被访问过的顶点
+            }
+            que.push(adjVet);       // 只入队未访问的顶点
+            visited.add(adjVet);    // 标记该顶点已被访问
+        }
+    }
+    // 返回顶点遍历序列
+    return res;
+}
+
+/* 初始化无向图 */
+const v = Vertex.valsToVets([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+const edges = [[v[0], v[1]], [v[0], v[3]], [v[1], v[2]], [v[1], v[4]],
+[v[2], v[5]], [v[3], v[4]], [v[3], v[6]], [v[4], v[5]],
+[v[4], v[7]], [v[5], v[8]], [v[6], v[7]], [v[7], v[8]]];
+const graph = new GraphAdjList(edges);
+console.log("\n初始化后，图为");
+graph.print();
+
+/* 广度优先遍历 BFS */
+const res = graphBfs(graph, v[0]);
+console.log("\n广度优先遍历（BFS）顶点序列为");
+console.log(Vertex.vetsToVals(res));
---
+++ codes/typescript/chapter_graph/graph_dfs.ts
@@ -0,0 +1,47 @@
+/**
+ * File: graph_dfs.ts
+ * Created Time: 2023-02-21
+ * Author: Zhuo Qinyue (1403450829@qq.com)
+ */
+
+
+import { Vertex } from '../module/Vertex';
+import { GraphAdjList } from './graph_adjacency_list';
+
+/* 深度优先遍历 DFS 辅助函数 */
+const dfs = (graph: GraphAdjList, visited: Set<Vertex>, res: Vertex[], vet: Vertex): void => {
+    res.push(vet);      // 记录访问顶点
+    visited.add(vet);   // 标记该顶点已被访问
+    // 遍历该顶点的所有邻接顶点
+    for (const adjVet of graph.adjList.get(vet)) {
+        if (visited.has(adjVet)) {
+            continue; // 跳过已被访问过的顶点
+        }
+        // 递归访问邻接顶点
+        dfs(graph, visited, res, adjVet);
+    }
+}
+
+/* 深度优先遍历 DFS */
+// 使用邻接表来表示图，以便获取指定顶点的所有邻接顶点
+const graphDFS = (graph: any, startVet: Vertex): Vertex[] => {
+    // 顶点遍历序列
+    const res: Vertex[] = [];
+    // 哈希表，用于记录已被访问过的顶点
+    const visited: Set<Vertex> = new Set();
+    dfs(graph, visited, res, startVet);
+    return res;
+}
+
+/* 初始化无向图 */
+const v: Vertex[] = Vertex.valsToVets([0, 1, 2, 3, 4, 5, 6]);
+const edges: Vertex[][] = [[v[0], v[1]], [v[0], v[3]], [v[1], v[2]],
+[v[2], v[5]], [v[4], v[5]], [v[5], v[6]]];
+const graph: GraphAdjList = new GraphAdjList(edges);
+console.log("\n初始化后，图为");
+graph.print();
+
+/* 深度优先遍历 DFS */
+const res: Vertex[] = graphDFS(graph, v[0]);
+console.log("\n深度优先遍历（DFS）顶点序列为");
+console.log(Vertex.vetsToVals(res));
