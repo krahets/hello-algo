6109f3234ce7f36666490caf7271cdaccf4c8695
steak-zhuo <zhuoqinyue@gmail.com>
2023-02-24 08:30:27 -0600

    update the type

--- codes/typescript/chapter_graph/graph_bfs.ts
+++ codes/typescript/chapter_graph/graph_bfs.ts
@@ -36,15 +36,15 @@ const graphBfs = (graph: GraphAdjList, startVet: Vertex): Vertex[] => {
 }
 
 /* 初始化无向图 */
-const v: Vertex[] = Vertex.valsToVets([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-const edges: Vertex[][] = [[v[0], v[1]], [v[0], v[3]], [v[1], v[2]], [v[1], v[4]],
+const v = Vertex.valsToVets([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+const edges = [[v[0], v[1]], [v[0], v[3]], [v[1], v[2]], [v[1], v[4]],
 [v[2], v[5]], [v[3], v[4]], [v[3], v[6]], [v[4], v[5]],
 [v[4], v[7]], [v[5], v[8]], [v[6], v[7]], [v[7], v[8]]];
-const graph: GraphAdjList = new GraphAdjList(edges);
+const graph = new GraphAdjList(edges);
 console.log("\n初始化后，图为");
 graph.print();
 
 /* 广度优先遍历 BFS */
-const res: Vertex[] = graphBfs(graph, v[0]);
+const res = graphBfs(graph, v[0]);
 console.log("\n广度优先遍历（BFS）顶点序列为");
 console.log(Vertex.vetsToVals(res));
--- codes/typescript/chapter_graph/graph_dfs.ts
+++ codes/typescript/chapter_graph/graph_dfs.ts
@@ -24,7 +24,7 @@ const dfs = (graph: GraphAdjList, visited: Set<Vertex>, res: Vertex[], vet: Vert
 
 /* 深度优先遍历 DFS */
 // 使用邻接表来表示图，以便获取指定顶点的所有邻接顶点
-const graphDFS = (graph: any, startVet: Vertex): Vertex[] => {
+const graphDFS = (graph: GraphAdjList, startVet: Vertex): Vertex[] => {
     // 顶点遍历序列
     const res: Vertex[] = [];
     // 哈希表，用于记录已被访问过的顶点
@@ -34,14 +34,14 @@ const graphDFS = (graph: any, startVet: Vertex): Vertex[] => {
 }
 
 /* 初始化无向图 */
-const v: Vertex[] = Vertex.valsToVets([0, 1, 2, 3, 4, 5, 6]);
-const edges: Vertex[][] = [[v[0], v[1]], [v[0], v[3]], [v[1], v[2]],
+const v = Vertex.valsToVets([0, 1, 2, 3, 4, 5, 6]);
+const edges = [[v[0], v[1]], [v[0], v[3]], [v[1], v[2]],
 [v[2], v[5]], [v[4], v[5]], [v[5], v[6]]];
-const graph: GraphAdjList = new GraphAdjList(edges);
+const graph = new GraphAdjList(edges);
 console.log("\n初始化后，图为");
 graph.print();
 
 /* 深度优先遍历 DFS */
-const res: Vertex[] = graphDFS(graph, v[0]);
+const res = graphDFS(graph, v[0]);
 console.log("\n深度优先遍历（DFS）顶点序列为");
 console.log(Vertex.vetsToVals(res));
