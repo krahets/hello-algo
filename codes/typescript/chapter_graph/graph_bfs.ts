ea582b9c6a4b0e60685822dbfb74610b95794cb5
Yudong Jin <krahets@163.com>
2023-02-25 01:08:53 +0800

    Update graph_bfs.ts

--- codes/typescript/chapter_graph/graph_bfs.ts
+++ codes/typescript/chapter_graph/graph_bfs.ts
@@ -38,8 +38,8 @@ const graphBfs = (graph: GraphAdjList, startVet: Vertex): Vertex[] => {
 /* 初始化无向图 */
 const v = Vertex.valsToVets([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 const edges = [[v[0], v[1]], [v[0], v[3]], [v[1], v[2]], [v[1], v[4]],
-[v[2], v[5]], [v[3], v[4]], [v[3], v[6]], [v[4], v[5]],
-[v[4], v[7]], [v[5], v[8]], [v[6], v[7]], [v[7], v[8]]];
+               [v[2], v[5]], [v[3], v[4]], [v[3], v[6]], [v[4], v[5]],
+               [v[4], v[7]], [v[5], v[8]], [v[6], v[7]], [v[7], v[8]]];
 const graph = new GraphAdjList(edges);
 console.log("\n初始化后，图为");
 graph.print();
