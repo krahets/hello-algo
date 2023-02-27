527fdc2409abb7982adcc53a3834e194d8213fb5
Yudong Jin <krahets@163.com>
2023-02-27 21:28:06 +0800

    Update list.py

--- codes/python/chapter_array_and_linkedlist/list.py
+++ codes/python/chapter_array_and_linkedlist/list.py
@@ -7,52 +7,52 @@ Author: Krahets (krahets@163.com)
 """ Driver Code """
 if __name__ == "__main__":
     """ 初始化列表 """
-    my_list = [1, 3, 2, 5, 4]
-    print("列表 my_list =", my_list)
+    list = [1, 3, 2, 5, 4]
+    print("列表 list =", list)
 
     """ 访问元素 """
-    num = my_list[1]
+    num = list[1]
     print("访问索引 1 处的元素，得到 num =", num)
 
     """ 更新元素 """
-    my_list[1] = 0
-    print("将索引 1 处的元素更新为 0 ，得到 my_list =", my_list)
+    list[1] = 0
+    print("将索引 1 处的元素更新为 0 ，得到 list =", list)
 
     """ 清空列表 """
-    my_list.clear()
-    print("清空列表后 my_list =", my_list)
+    list.clear()
+    print("清空列表后 list =", list)
 
     """ 尾部添加元素 """
-    my_list.append(1)
-    my_list.append(3)
-    my_list.append(2)
-    my_list.append(5)
-    my_list.append(4)
-    print("添加元素后 my_list =", my_list)
+    list.append(1)
+    list.append(3)
+    list.append(2)
+    list.append(5)
+    list.append(4)
+    print("添加元素后 list =", list)
 
     """ 中间插入元素 """
-    my_list.insert(3, 6)
-    print("在索引 3 处插入数字 6 ，得到 my_list =", my_list)
+    list.insert(3, 6)
+    print("在索引 3 处插入数字 6 ，得到 list =", list)
 
     """ 删除元素 """
-    my_list.pop(3)
-    print("删除索引 3 处的元素，得到 my_list =", my_list)
+    list.pop(3)
+    print("删除索引 3 处的元素，得到 list =", list)
 
     """ 通过索引遍历列表 """
     count = 0
-    for i in range(len(my_list)):
+    for i in range(len(list)):
         count += 1
 
     """ 直接遍历列表元素 """
     count = 0
-    for n in my_list:
+    for n in list:
         count += 1
 
     """ 拼接两个列表 """
-    new_list = [6, 8, 7, 10, 9]
-    my_list += new_list
-    print("将列表 new_list 拼接到 my_list 之后，得到 my_list =", my_list)
+    list1 = [6, 8, 7, 10, 9]
+    list += list1
+    print("将列表 list1 拼接到 list 之后，得到 list =", list)
 
     """ 排序列表 """
-    my_list.sort()
-    print("排序列表后 my_list =", my_list)
+    list.sort()
+    print("排序列表后 list =", list)
