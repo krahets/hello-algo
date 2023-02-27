327f385d3250b6557005a7d818f6108d2b4e0329
Reanon <793584285@qq.com>
2023-02-27 20:34:30 +0800

    feat(go/graph): add go code for graph_dfs/bfs (#372)
    * feat(vertex): add a vertex pkg
    
    * feat(graph): add graph_bfs in go
    
    * feat(graph): add graph_dfs in go
    
    * fix(graph): fix comment
    
    * fix(graph): fix graph_adj_list
    
    * fix(go/graph): fix graph_adjacency
    
    * fix(c): gitignore
    
    * feat(graph): print order adjList graph
    
    * fix(graph): remove order print
    
    * Update graph_adjacency_list_test.go
    
    * Update .gitignore
    
    * Update .gitignore
    
    ---------
    
    Co-authored-by: Yudong Jin <krahets@163.com>

--- codes/go/chapter_graph/graph_adjacency_list_test.go
+++ codes/go/chapter_graph/graph_adjacency_list_test.go
@@ -7,41 +7,39 @@ package chapter_graph
 import (
 	"fmt"
 	"testing"
+
+	. "github.com/krahets/hello-algo/pkg"
 )
 
 func TestGraphAdjList(t *testing.T) {
 	/* 初始化无向图 */
-	v0 := newVertex(1)
-	v1 := newVertex(3)
-	v2 := newVertex(2)
-	v3 := newVertex(5)
-	v4 := newVertex(4)
-	edges := [][]vertex{{v0, v1}, {v1, v2}, {v2, v3}, {v0, v3}, {v2, v4}, {v3, v4}}
+	v := ValsToVets([]int{1, 3, 2, 5, 4})
+	edges := [][]Vertex{{v[0], v[1]}, {v[0], v[3]}, {v[1], v[2]}, {v[2], v[3]}, {v[2], v[4]}, {v[3], v[4]}}
 	graph := newGraphAdjList(edges)
 	fmt.Println("初始化后，图为:")
 	graph.print()
 
 	/* 添加边 */
-	// 顶点 1, 2 即 v0, v2
-	graph.addEdge(v0, v2)
+	// 顶点 1, 2 即 v[0], v[2]
+	graph.addEdge(v[0], v[2])
 	fmt.Println("\n添加边 1-2 后，图为")
 	graph.print()
 
 	/* 删除边 */
-	// 顶点 1, 3 即 v0, v1
-	graph.removeEdge(v0, v1)
+	// 顶点 1, 3 即 v[0], v[1]
+	graph.removeEdge(v[0], v[1])
 	fmt.Println("\n删除边 1-3 后，图为")
 	graph.print()
 
 	/* 添加顶点 */
-	v5 := newVertex(6)
+	v5 := NewVertex(6)
 	graph.addVertex(v5)
 	fmt.Println("\n添加顶点 6 后，图为")
 	graph.print()
 
 	/* 删除顶点 */
-	// 顶点 3 即 v1
-	graph.removeVertex(v1)
+	// 顶点 3 即 v[1]
+	graph.removeVertex(v[1])
 	fmt.Println("\n删除顶点 3 后，图为")
 	graph.print()
 }
