5406d9d808583ac38b6763dd1a57e90a3150ebef
hpstory <354195783@qq.com>
2023-03-07 14:02:29 +0800

    add csharp code graph_bfs and graph_dfs, modify graph adjacency list class inaccessable

--- codes/csharp/chapter_graph/graph_adjacency_list.cs
+++ codes/csharp/chapter_graph/graph_adjacency_list.cs
@@ -10,10 +10,10 @@ using NUnit.Framework;
 namespace hello_algo.chapter_graph;
 
 /* 基于邻接表实现的无向图类 */
-class GraphAdjList
+public class GraphAdjList
 {
     // 邻接表，key: 顶点，value：该顶点的所有邻接顶点
-    Dictionary<Vertex, List<Vertex>> adjList;
+    public Dictionary<Vertex, List<Vertex>> adjList;
 
     /* 构造函数 */
     public GraphAdjList(Vertex[][] edges)
