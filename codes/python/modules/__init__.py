0a4a846e1319af3ed4f4da7ad86d5e5d5e1de18b
krahets <krahets@163.com>
2023-03-23 18:56:14 +0800

    Fix python modules

--- codes/python/modules/__init__.py
+++ codes/python/modules/__init__.py
@@ -3,6 +3,6 @@
 from __future__ import annotations
 # Import common libs here to simplify the codes by `from module import *`
 from .linked_list import ListNode, list_to_linked_list, linked_list_to_list, get_list_node
-from .binary_tree import TreweNode, list_to_tree, tree_to_list, get_tree_node
+from .binary_tree import TreeNode, list_to_tree, tree_to_list, get_tree_node
 from .vertex import Vertex, vals_to_vets, vets_to_vals
 from .print_util import print_matrix, print_linked_list, print_tree, print_dict, print_heap
