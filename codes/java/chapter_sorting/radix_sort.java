8962ef08911edf7533182c2c6627f79ce86aecb9
krahets <krahets@163.com>
2023-03-26 21:59:55 +0800

    Add the section of radix sort.

--- codes/java/chapter_sorting/radix_sort.java
+++ codes/java/chapter_sorting/radix_sort.java
@@ -16,51 +16,51 @@ public class radix_sort {
     }
 
     /* 计数排序（根据 nums 第 k 位排序） */
-    static void countingSort(int[] nums, int exp) {
-        // 十进制的各位数字范围为 0~9 ，因此需要长度为 10 的桶
-        int[] bucket = new int[10];
+    static void countingSortDigit(int[] nums, int exp) {
+        // 十进制的位范围为 0~9 ，因此需要长度为 10 的桶
+        int[] counter = new int[10];
         int n = nums.length;
-        // 借助桶来统计 0~9 各数字的出现次数
+        // 统计 0~9 各数字的出现次数
         for (int i = 0; i < n; i++) {
             int d = digit(nums[i], exp); // 获取 nums[i] 第 k 位，记为 d
-            bucket[d]++;                 // 统计数字 d 的出现次数
+            counter[d]++;                // 统计数字 d 的出现次数
         }
         // 求前缀和，将“出现个数”转换为“数组索引”
         for (int i = 1; i < 10; i++) {
-            bucket[i] += bucket[i - 1];
+            counter[i] += counter[i - 1];
         }
-        // 倒序遍历，根据桶内统计结果，将各元素填入暂存数组 tmp
-        int[] tmp = new int[n];
+        // 倒序遍历，根据桶内统计结果，将各元素填入 res
+        int[] res = new int[n];
         for (int i = n - 1; i >= 0; i--) {
             int d = digit(nums[i], exp);
-            int j = bucket[d] - 1; // 获取 d 在数组中的索引 j
-            tmp[j] = nums[i];      // 将当前元素填入索引 j
-            bucket[d]--;           // 将 d 的数量减 1
+            int j = counter[d] - 1; // 获取 d 在数组中的索引 j
+            res[j] = nums[i];       // 将当前元素填入索引 j
+            counter[d]--;           // 将 d 的数量减 1
         }
-        // 将 tmp 复制到 nums
+        // 使用结果覆盖原数组 nums
         for (int i = 0; i < n; i++)
-            nums[i] = tmp[i];
+            nums[i] = res[i];
     }
 
     /* 基数排序 */
     static void radixSort(int[] nums) {
         // 获取数组的最大元素，用于判断最大位数
-        int ma = Integer.MIN_VALUE;
+        int m = Integer.MIN_VALUE;
         for (int num : nums)
-            if (num > ma) ma = num;
+            if (num > m) m = num;
         // 按照从低位到高位的顺序遍历
-        for (int exp = 1; ma >= exp; exp *= 10)
-            // 对数组元素的第 k 位执行「计数排序」
+        for (int exp = 1; exp <= m; exp *= 10)
+            // 对数组元素的第 k 位执行计数排序
             // k = 1 -> exp = 1
             // k = 2 -> exp = 10
-            // k = 3 -> exp = 100
             // 即 exp = 10^(k-1)
-            countingSort(nums, exp);
+            countingSortDigit(nums, exp);
     }
 
     public static void main(String[] args) {
-        /* 基数排序 */
-        int[] nums = { 23, 12, 3, 4, 788, 192 };
+        // 基数排序
+        int[] nums = { 10546151, 35663510, 42865989, 34862445, 81883077, 
+                       88906420, 72429244, 30524779, 82060337, 63832996 };
         radixSort(nums);
         System.out.println("基数排序完成后 nums = " + Arrays.toString(nums));
     }
