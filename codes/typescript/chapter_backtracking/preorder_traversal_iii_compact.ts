8908b85e739f6eaef8a7a5ca2c3edbdc23657e29
Justin Tse <xiefahit@gmail.com>
2023-05-11 22:06:57 +0800

    Update preorder_traversal_iii_compact.ts

--- codes/typescript/chapter_backtracking/preorder_traversal_iii_compact.ts
+++ codes/typescript/chapter_backtracking/preorder_traversal_iii_compact.ts
@@ -9,7 +9,7 @@ import { arrToTree } from '../modules/TreeNode';
 import { printTree } from '../modules/PrintUtil';
 
 /* 前序遍历：例题三 */
-function preOrder(root, path, res) {
+function preOrder(root: TreeNode | null, path: TreeNode[], res: TreeNode[][]): void {
   // 剪枝
   if (root === null || root.val === 3) {
     return;
@@ -32,8 +32,8 @@ console.log("\n初始化二叉树");
 printTree(root);
 
 // 前序遍历
-const path = [];
-const res = [];
+const path: TreeNode[] = [];
+const res: TreeNode[][] = [];
 preOrder(root, path, res);
 
 console.log("\n输出所有根节点到节点 7 的路径，且路径中不包含值为 3 的节点");
