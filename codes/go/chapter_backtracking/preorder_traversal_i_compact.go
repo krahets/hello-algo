3a0b1df46db473412d2d1a9ef83681f681a7a251
reanon <793584285@qq.com>
2023-05-14 19:20:03 +0800

    feat(backtracking): add /preorder_traversal_i_compact in go

--- codes/go/chapter_backtracking/preorder_traversal_i_compact.go
+++ codes/go/chapter_backtracking/preorder_traversal_i_compact.go
@@ -5,36 +5,18 @@
 package chapter_backtracking
 
 import (
-	"fmt"
-	"testing"
-
 	. "github.com/krahets/hello-algo/pkg"
 )
 
-var res = make([]any, 0)
-
 /* 前序遍历：例题一 */
-func preOrder(root *TreeNode) {
+func preOrderI(root *TreeNode, res *[]*TreeNode) {
 	if root == nil {
 		return
 	}
 	if int(root.Val) == 7 {
 		// 记录解
-		res = append(res, root.Val)
+		*res = append(*res, root)
 	}
-	preOrder(root.Left)
-	preOrder(root.Right)
-}
-
-func TestPreorderTraversalICompact(t *testing.T) {
-	/* 初始化二叉树 */
-	root := ArrToTree([]any{1, 7, 3, 4, 5, 6, 7})
-	fmt.Println("\n初始化二叉树")
-	PrintTree(root)
-
-	// 前序遍历
-	preOrder(root)
-
-	fmt.Println("\n输出所有值为 7 的节点")
-	PrintSlice(res)
+	preOrderI(root.Left, res)
+	preOrderI(root.Right, res)
 }
