dc49fdf75e2b9161a98f8491097f8f4db7a8b12a
krahets <krahets@163.com>
2023-05-24 11:45:52 +0800

    Polish some contents.

--- codes/cpp/chapter_sorting/selection_sort.cpp
+++ codes/cpp/chapter_sorting/selection_sort.cpp
@@ -11,12 +11,11 @@ void selectionSort(vector<int> &nums) {
     int n = nums.size();
     // 外循环：未排序区间为 [i, n-1]
     for (int i = 0; i < n - 1; i++) {
-        // 内循环：找到未排序区间 [i, n-1] 中的最小元素
+        // 内循环：找到未排序区间内的最小元素
         int k = i;
         for (int j = i + 1; j < n; j++) {
-            if (nums[j] < nums[k]) {
-                k = j; // 更新最小元素
-            }
+            if (nums[j] < nums[k])
+                k = j; // 记录最小元素的索引
         }
         // 将该最小元素与未排序区间的首个元素交换
         swap(nums[i], nums[k]);
--- codes/python/chapter_sorting/selection_sort.py
+++ codes/python/chapter_sorting/selection_sort.py
@@ -10,11 +10,11 @@ def selection_sort(nums: list[int]):
     n = len(nums)
     # 外循环：未排序区间为 [i, n-1]
     for i in range(n - 1):
-        # 内循环：找到未排序区间 [i, n-1] 中的最小元素
+        # 内循环：找到未排序区间内的最小元素
         k = i
         for j in range(i + 1, n):
             if nums[j] < nums[k]:
-                k = j  # 更新最小元素
+                k = j  # 记录最小元素的索引
         # 将该最小元素与未排序区间的首个元素交换
         nums[i], nums[k] = nums[k], nums[i]
 
