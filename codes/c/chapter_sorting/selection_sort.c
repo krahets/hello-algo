0892aa9a1b8219d8861f7351cb966b31bd8dbbb9
Gonglja <glj0@outlook.com>
2023-05-31 00:25:24 +0800

    feat(codes/c): Add selection_sort.c

---
+++ codes/c/chapter_sorting/selection_sort.c
@@ -0,0 +1,43 @@
+/**
+ * File: selection_sort.c
+ * Created Time: 2023-05-31
+ * Author: Gonglja (glj0@outlook.com)
+ */
+
+#include "../utils/common.h"
+
+/* 交换两个元素的值 */
+void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+/* 选择排序 */
+void selectionSort(int nums[], int n) {
+    // 外循环：未排序区间为 [i, n-1]
+    for (int i = 0; i < n - 1; i++) {
+        // 内循环：找到未排序区间内的最小元素
+        int k = i;
+        for (int j = i + 1; j < n; j++) {
+            if (nums[j] < nums[k])
+                k = j; // 记录最小元素的索引
+        }
+        // 将该最小元素与未排序区间的首个元素交换
+        swap(&nums[i], &nums[k]);
+    }
+}
+
+
+/* 主函数 */
+int main() {
+    int nums[] = {4, 1, 3, 1, 5, 2};
+    int n = sizeof(nums) / sizeof(nums[0]);
+
+    selectionSort(nums, n);
+
+    printf("选择排序完成后 nums = ");
+    printArray(nums, n);
+
+    return 0;
+}
