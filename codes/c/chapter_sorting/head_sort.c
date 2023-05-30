5034e02ea58d8f70b331f30361cf9fd20d28b802
Gonglja <glj0@outlook.com>
2023-05-30 21:44:28 +0800

    feat(codes/c): Add head_sort.c

---
+++ codes/c/chapter_sorting/head_sort.c
@@ -0,0 +1,63 @@
+/**
+ * File: heap_sort.c
+ * Created Time: 2023-05-30
+ * Author: Gonglja (glj0@outlook.com)
+ */
+
+#include "../utils/common.h"
+
+void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+/* 堆的长度为 n ，从节点 i 开始，从顶至底堆化 */
+void siftDown(int nums[], int n, int i) {
+    while (1) {
+        // 判断节点 i, l, r 中值最大的节点，记为 ma
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        int ma = i;
+        if (l < n && nums[l] > nums[ma])
+            ma = l;
+        if (r < n && nums[r] > nums[ma])
+            ma = r;
+        // 若节点 i 最大或索引 l, r 越界，则无需继续堆化，跳出
+        if (ma == i) {
+            break;
+        }
+        // 交换两节点
+        swap(&nums[i], &nums[ma]);
+        // 循环向下堆化
+        i = ma;
+    }
+}
+
+/* 堆排序 */
+void heapSort(int nums[], int n) {
+    // 建堆操作：堆化除叶节点以外的其他所有节点
+    for (int i = n / 2 - 1; i >= 0; --i) {
+        siftDown(nums, n, i);
+    }
+    // 从堆中提取最大元素，循环 n-1 轮
+    for (int i = n - 1; i > 0; --i) {
+        // 交换根节点与最右叶节点（即交换首元素与尾元素）
+        swap(&nums[0], &nums[i]);
+        // 以根节点为起点，从顶至底进行堆化
+        siftDown(nums, i, 0);
+    }
+}
+
+
+/* Driver Code */
+int main() {
+    int nums[] = {4, 1, 3, 1, 5, 2};
+    int n = sizeof(nums) / sizeof(nums[0]);
+
+    heapSort(nums, n);
+    printf("堆排序完成后 nums = ");
+    printArray(nums, n);
+
+    return 0;
+}
