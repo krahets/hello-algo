ba9ef1f5a6a2f726b194901794ceefc110d3d83d
reanon <793584285@qq.com>
2023-06-23 22:27:29 +0800

    feat(go): support hash map chaining

--- codes/go/chapter_hashing/hash_map_test.go
+++ codes/go/chapter_hashing/hash_map_test.go
@@ -13,43 +13,69 @@ import (
 
 func TestHashmap(t *testing.T) {
 	/* 初始化哈希表 */
-	mapp := make(map[int]string)
+	hmap := make(map[int]string)
 
 	/* 添加操作 */
 	// 在哈希表中添加键值对 (key, value)
-	mapp[12836] = "小哈"
-	mapp[15937] = "小啰"
-	mapp[16750] = "小算"
-	mapp[13276] = "小法"
-	mapp[10583] = "小鸭"
+	hmap[12836] = "小哈"
+	hmap[15937] = "小啰"
+	hmap[16750] = "小算"
+	hmap[13276] = "小法"
+	hmap[10583] = "小鸭"
 	fmt.Println("\n添加完成后，哈希表为\nKey -> Value")
-	PrintMap(mapp)
+	PrintMap(hmap)
 
 	/* 查询操作 */
 	// 向哈希表输入键 key ，得到值 value
-	name := mapp[15937]
+	name := hmap[15937]
 	fmt.Println("\n输入学号 15937 ，查询到姓名 ", name)
 
 	/* 删除操作 */
 	// 在哈希表中删除键值对 (key, value)
-	delete(mapp, 10583)
+	delete(hmap, 10583)
 	fmt.Println("\n删除 10583 后，哈希表为\nKey -> Value")
-	PrintMap(mapp)
+	PrintMap(hmap)
 
 	/* 遍历哈希表 */
 	// 遍历键值对 key->value
 	fmt.Println("\n遍历键值对 Key->Value")
-	for key, value := range mapp {
+	for key, value := range hmap {
 		fmt.Println(key, "->", value)
 	}
 	// 单独遍历键 key
 	fmt.Println("\n单独遍历键 Key")
-	for key := range mapp {
+	for key := range hmap {
 		fmt.Println(key)
 	}
 	// 单独遍历值 value
 	fmt.Println("\n单独遍历值 Value")
-	for _, value := range mapp {
+	for _, value := range hmap {
 		fmt.Println(value)
 	}
+}
+
+func TestHashMapChaining(t *testing.T) {
+	/* 初始化哈希表 */
+	hmap := newHashMapChaining()
+
+	/* 添加操作 */
+	// 在哈希表中添加键值对 (key, value)
+	hmap.put(12836, "小哈")
+	hmap.put(15937, "小啰")
+	hmap.put(16750, "小算")
+	hmap.put(13276, "小法")
+	hmap.put(10583, "小鸭")
+	fmt.Println("\n添加完成后，哈希表为\nKey -> Value")
+	hmap.print()
+
+	/* 查询操作 */
+	// 向哈希表输入键 key ，得到值 value
+	name := hmap.get(15937)
+	fmt.Println("\n输入学号 15937 ，查询到姓名 ", name)
+
+	/* 删除操作 */
+	// 在哈希表中删除键值对 (key, value)
+	hmap.remove(12836)
+	fmt.Println("\n删除 12836 后，哈希表为\nKey -> Value")
+	hmap.print()
 }
