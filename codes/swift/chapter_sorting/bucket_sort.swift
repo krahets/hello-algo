9611a8f135385f8fd8ff73f30506e6e470bcdce5
nuomi1 <nuomi1@qq.com>
2023-06-30 05:05:07 +0800

    fix: compiler error and reset root (#570)

--- codes/swift/chapter_sorting/bucket_sort.swift
+++ codes/swift/chapter_sorting/bucket_sort.swift
@@ -12,7 +12,7 @@ func bucketSort(nums: inout [Double]) {
     // 1. 将数组元素分配到各个桶中
     for num in nums {
         // 输入数据范围 [0, 1)，使用 num * k 映射到索引范围 [0, k-1]
-        let i = Int(num * k)
+        let i = Int(num * Double(k))
         // 将 num 添加进桶 i
         buckets[i].append(num)
     }
