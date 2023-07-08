4018b374f0b1f4a4c5af981c4e14fd3a0b20a0e5
NI-SW <80197588+NI-SW@users.noreply.github.com>
2023-07-08 16:08:18 +0800

    Update format and output

--- codes/c/chapter_graph/graph_adjacency_list.c
+++ codes/c/chapter_graph/graph_adjacency_list.c
@@ -59,12 +59,6 @@ struct Link_list
 {
     Node* head;
     Node* tail;
-    void (*push_front)(struct Link_list* l, Vertex* val);
-    void (*push_back)(struct Link_list* l, Vertex* val);
-    void (*remove_node)(struct Link_list* l, Vertex* val);
-    void (*remove_edge)(struct Link_list* l, Vertex* val);
-    void (*free_Link_list)(struct Link_list* l);
-    Node* (*find_by_index)(struct Link_list* l, unsigned int index);
 };
 
 typedef struct Link_list Link_list;  
@@ -177,24 +171,15 @@ void free_Link_list(Link_list* l)
 }
 Link_list* new_Linklist(Vertex* v)
 {
-    Link_list* l = (Link_list*)malloc(sizeof(Link_list));
-    l->remove_edge = remove_edge;
-    l->push_back = push_back;
-    l->push_front = push_front;
-    l->remove_node = remove_node;
-    l->free_Link_list = free_Link_list;
-    l->find_by_index = find_by_index;
+    Link_list* newLinklist = (Link_list*)malloc(sizeof(Link_list));
 
-    l->head = new_node();
-    l->head->val = v;
-    l->tail = l->head;
-    l->head->next = 0;
+    newLinklist->head = new_node();
+    newLinklist->head->val = v;
+    newLinklist->tail = newLinklist->head;
+    newLinklist->head->next = 0;
 
-    return l;
+    return newLinklist;
 }
- 
-
-
 
 /* 基于邻接链表实现的无向图类结构 */
 struct graphAdjList
@@ -203,12 +188,6 @@ struct graphAdjList
     Link_list* vertices_list;
     // 顶点数量
     unsigned int size;
-    void (*addVertex)(graphAdjList* t, int val);
-    void (*addEdge)(graphAdjList* t, int i, int j);
-    void (*removeVertex)(graphAdjList* t, unsigned int index);
-    void (*removeEdge)(graphAdjList* t, int i, int j);
-
-    void (*print_graph)(graphAdjList* t);
 };
 
 typedef struct graphAdjList graphAdjList;
@@ -217,17 +196,17 @@ typedef struct graphAdjList graphAdjList;
 void addEdge(graphAdjList* t, int i, int j)
 {
     // 越界检查
-    if(i >= t->size || j >= t->size || i < 0 || j < 0 || i == j)
+    if(i < 0 || j < 0 || i == j || i >= t->size || j >= t->size)
     {
         printf("Out of range in %s:%d\n", __FILE__, __LINE__);
         return;
     }
     // 查找待连接的节点
-    Node* v1 = t->vertices_list->find_by_index(t->vertices_list, i);
-    Node* v2 = t->vertices_list->find_by_index(t->vertices_list, j);
+    Node* v1 = find_by_index(t->vertices_list, i);
+    Node* v2 = find_by_index(t->vertices_list, j);
     // 连接节点
-    v1->val->linked->push_front(v1->val->linked, v2->val);
-    v2->val->linked->push_front(v2->val->linked, v1->val);
+    push_front(v1->val->linked, v2->val);
+    push_front(v2->val->linked, v1->val);
 
 }
 
@@ -235,18 +214,18 @@ void addEdge(graphAdjList* t, int i, int j)
 void removeEdge(graphAdjList* t, int i, int j) 
 {
     // 越界检查
-    if(i >= t->size || j >= t->size || i < 0 || j < 0 || i == j)
+    if(i < 0 || j < 0 || i == j || i >= t->size || j >= t->size)
     {
         printf("Out of range in %s:%d\n", __FILE__, __LINE__);
         return;
     }
     
     // 查找待删除边的相关节点
-    Node* v1 = t->vertices_list->find_by_index(t->vertices_list, i);
-    Node* v2 = t->vertices_list->find_by_index(t->vertices_list, j);
+    Node* v1 = find_by_index(t->vertices_list, i);
+    Node* v2 = find_by_index(t->vertices_list, j);
     // 移除待删除边
-    v1->val->linked->remove_edge(v1->val->linked, v2->val);
-    v2->val->linked->remove_edge(v2->val->linked, v1->val);
+    remove_edge(v1->val->linked, v2->val);
+    remove_edge(v2->val->linked, v1->val);
 }
 
 /* 添加顶点 */
@@ -255,7 +234,7 @@ void addVertex(graphAdjList* t, int val)
     // 声明新顶点
     t->size++;
     Vertex* temp = new_Vertex(val);
-    t->vertices_list->push_front(t->vertices_list, temp);
+    push_front(t->vertices_list, temp);
 }
 
 /* 删除顶点 */
@@ -272,7 +251,7 @@ void removeVertex(graphAdjList* t, unsigned int index)
     //Node* temp = t->vertices_list->head->next;
 
     // 查找待删节点
-    Node* v = t->vertices_list->find_by_index(t->vertices_list, index);
+    Node* v = find_by_index(t->vertices_list, index);
     // 若不存在该节点，则返回
     if(v == 0)
     {
@@ -285,20 +264,20 @@ void removeVertex(graphAdjList* t, unsigned int index)
     Node* temp = v->val->linked->head->next;
     while(temp != 0)
     {
-        temp->val->linked->remove_edge(temp->val->linked, v->val);
+        remove_edge(temp->val->linked, v->val);
         temp = temp->next;
     }
 
     // 移除待删除结点
-    t->vertices_list->remove_node(t->vertices_list, v->val);
+    remove_node(t->vertices_list, v->val);
     t->size--;
 }
 
 /* 打印顶点与邻接矩阵 */
-void print_graph(graphAdjList* t)
+void printGraph(graphAdjList* t)
 {
     Node* temp = t->vertices_list->head->next;
-    printf("顶点  边\n");
+    printf("邻接表  =\n");
     while(temp != 0)
     {
         Node* t = temp->val->linked->head->next;
@@ -321,22 +300,13 @@ void print_graph(graphAdjList* t)
 }
 
 /* 构造函数 */
-graphAdjList* new_graphic()
+graphAdjList* newGraphic()
 {    
     // 申请内存
-    graphAdjList* t = (graphAdjList*)malloc(sizeof(graphAdjList));
+    graphAdjList* newGraph = (graphAdjList*)malloc(sizeof(graphAdjList));
     // 建立节点表
-    t->vertices_list = new_Linklist(0);
-
-    // 函数指针
-    t->addVertex = addVertex;
-    t->addEdge = addEdge;
-    t->removeVertex = removeVertex;
-    t->removeEdge = removeEdge;
-    t->print_graph = print_graph;    
-
-
-
+    newGraph->vertices_list = new_Linklist(0);
+    return newGraph;
 }
 
 /* Driver Code */
@@ -344,44 +314,44 @@ int main()
 {
 
     // 构造初始图结构
-    graphAdjList* graph = new_graphic();
-    graph->addVertex(graph,4);    
-    graph->addVertex(graph,5);
-    graph->addVertex(graph,2);
-    graph->addVertex(graph,3);
-    graph->addVertex(graph,1);
+    graphAdjList* graph = newGraphic();
+    addVertex(graph,4);    
+    addVertex(graph,5);
+    addVertex(graph,2);
+    addVertex(graph,3);
+    addVertex(graph,1);
 
-    graph->addEdge(graph,0,1);
-    graph->addEdge(graph,0,3);
-    graph->addEdge(graph,1,2);
-    graph->addEdge(graph,2,3);
-    graph->addEdge(graph,2,4);
-    graph->addEdge(graph,3,4);
+    addEdge(graph,0,1);
+    addEdge(graph,0,3);
+    addEdge(graph,1,2);
+    addEdge(graph,2,3);
+    addEdge(graph,2,4);
+    addEdge(graph,3,4);
     printf("初始化后，图为:\n");
-    graph->print_graph(graph);
+    printGraph(graph);
 
     /* 添加边 */
     // 顶点 1, 2 的索引分别为 0, 2
-    graph->addEdge(graph,0,2);
+    addEdge(graph,0,2);
     printf("添加边 1-2 后图为\n");
-    graph->print_graph(graph);
+    printGraph(graph);
 
     /* 删除边 */
     // 顶点 1, 3 的索引分别为 0, 1
-    graph->removeEdge(graph,0,1);
+    removeEdge(graph,0,1);
     printf("删除边 1-3 后，图为\n");
-    graph->print_graph(graph);
+    printGraph(graph);
 
     /* 添加顶点 */
-    graph->addVertex(graph,6);
+    addVertex(graph,6);
     printf("添加顶点 6 后，图为\n");
-    graph->print_graph(graph);
+    printGraph(graph);
 
     /* 删除顶点 */
     // 顶点 3 的索引为 2
-    graph->removeVertex(graph,2);
+    removeVertex(graph,2);
     printf("删除顶点 3 后，图为\n");
-    graph->print_graph(graph);
+    printGraph(graph);
 
     return 0;
 }
