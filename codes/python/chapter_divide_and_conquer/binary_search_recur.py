1f784dadb076862ed66fed36539fa489b5c7b41e
krahets <krahets@163.com>
2023-07-17 04:20:12 +0800

    Add Java and C++ code for the chapter of divide and conquer.

--- codes/python/chapter_divide_and_conquer/binary_search_recur.py
+++ codes/python/chapter_divide_and_conquer/binary_search_recur.py
@@ -1,22 +1,22 @@
 """
 File: binary_search_recur.py
 Created Time: 2023-07-17
-Author: krahets (xisunyy@163.com)
+Author: krahets (krahets@163.com)
 """
 
 
 def dfs(nums: list[int], target: int, i: int, j: int) -> int:
-    """二分查找：分治"""
-    # 若区间为空，代表未找到目标元素，则返回 -1
+    """二分查找：问题 f(i, j)"""
+    # 若区间为空，代表无目标元素，则返回 -1
     if i > j:
         return -1
     # 计算中点索引 m
     m = (i + j) // 2
     if nums[m] < target:
-        # 此情况说明 target 在区间 [m+1, j] 中，递归解决该子问题
+        # 递归子问题 f(m+1, j)
         return dfs(nums, target, m + 1, j)
     elif nums[m] > target:
-        # 此情况说明 target 在区间 [i, m-1] 中，递归解决该子问题
+        # 递归子问题 f(i, m-1)
         return dfs(nums, target, i, m - 1)
     else:
         # 找到目标元素，返回其索引
@@ -26,6 +26,7 @@ def dfs(nums: list[int], target: int, i: int, j: int) -> int:
 def binary_search(nums: list[int], target: int) -> int:
     """二分查找"""
     n = len(nums)
+    # 求解问题 f(0, n-1)
     return dfs(nums, target, 0, n - 1)
 
 
