c0ac1eb8453bab7ddb9fa74839c33d47fd20df7f
张鸿运 <1836017030@qq.com>
2023-07-19 09:17:41 +0800

    feat: Add Go code to binary search recursion under divide and conquer

---
+++ codes/go/chapter_divide_and_conquer/binary_search_recur_test.go
@@ -0,0 +1,19 @@
+// File: binary_search_recur_test.go
+// Created Time: 2023-07-19
+// Author: hongyun-robot (1836017030@qq.com)
+package chapter_divide_and_conquer
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBinarySearch(t *testing.T) {
+	nums := []int{1, 3, 6, 8, 12, 15, 23, 26, 31, 35}
+	target := 6
+	noTarget := 99
+	targetIndex := binarySearch(nums, target)
+	fmt.Println("target index = ", targetIndex)
+	noTargetIndex := binarySearch(nums, noTarget)
+	fmt.Println("noTarget index = ", noTargetIndex)
+}
