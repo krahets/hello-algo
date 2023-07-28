7c93668b40dbfae65a0c590ea790e0c9a563c7b1
Gonglja <glj0@outlook.com>
2023-07-28 23:42:09 +0800

    feat(codes/c): Remove myArray, use public vector.

--- codes/c/chapter_backtracking/preorder_traversal_i_compact.c
+++ codes/c/chapter_backtracking/preorder_traversal_i_compact.c
@@ -6,53 +6,13 @@
 
 #include "../utils/common.h"
 
-#define MaxLength 1024
+vector *res;
 
-/* TreeNode 动态数组简单实现 */
-struct myArray {
-    TreeNode *(data[MaxLength]);
-    int size;
-};
-
-typedef struct myArray myArray;
-
-myArray *newArray() {
-    myArray *array = malloc(sizeof(myArray));
-    array->size = 0;
+// 打印向量中的元素
+void printFunc(vector *v, void *p) {
+    TreeNode *node = p;
+    printf("%d ", node->val);
 }
-
-void delArray(myArray *array) {
-    if (array) {
-        for (int i = 0; i < array->size && array->data[i]; ++i) {
-            free(array->data[i]);
-        }
-
-        array->size = 0;
-        free(array);
-    }
-}
-
-void pushBack(myArray *array, TreeNode *newNode) {
-    if (array) {
-        if (array->data)
-            array->data[array->size] = newNode;
-        array->size++;
-    }
-}
-
-void printMyArray(myArray *array) {
-    printf("[");
-    for (int i = 0; i < array->size && array->data[i]; ++i) {
-        printf("%d", array->data[i]->val);
-        if (i == array->size - 1) {
-            printf("]");
-        } else {
-            printf(", ");
-        }
-    }
-}
-
-myArray *res;
 
 /* 前序遍历：例题一 */
 static void preOrder(TreeNode *root) {
@@ -61,7 +21,7 @@ static void preOrder(TreeNode *root) {
     }
     if (root->val == 7) {
         // 记录解
-        pushBack(res, root);
+        vectorPushback(res, root);
     }
     preOrder(root->left);
     preOrder(root->right);
@@ -70,7 +30,7 @@ static void preOrder(TreeNode *root) {
 /* Driver Code */
 int main() {
     int arr[] = {1, 7, 3, 4, 5, 6, 7};
-    res = newArray();
+    res = newVector();
     TreeNode *root = arrToTree(arr, sizeof(arr) / sizeof(arr[0]));
     printf("\n初始化二叉树\r\n");
     printTree(root);
@@ -79,7 +39,6 @@ int main() {
     preOrder(root);
 
     printf("\n输出所有值为 7 的节点\r\n");
-    printMyArray(res);
-
-    delArray(res);
+    printVector(res, printFunc);
+    delVector(res);
 }
