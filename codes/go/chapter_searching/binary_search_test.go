45d569b6336303b3c4c30a0600c87c81e91cf4d3
krahets <krahets@163.com>
2023-08-04 05:14:52 +0800

    Add the section of binary search insertion. Refactor the section of binary search edge. Finetune the figures of binary search.

--- codes/go/chapter_searching/binary_search_test.go
+++ codes/go/chapter_searching/binary_search_test.go
@@ -22,15 +22,3 @@ func TestBinarySearch(t *testing.T) {
 		t.Errorf("目标元素 6 的索引 = %d, 应该为 %d", actual, expected)
 	}
 }
-
-func TestBinarySearchEdge(t *testing.T) {
-	target := 6
-	nums := []int{1, 3, 6, 6, 6, 6, 6, 10, 12, 15}
-	// 二分查找最左一个元素
-	indexLeft := binarySearchLeftEdge(nums, target)
-	fmt.Println("数组中最左一个元素 6 的索引 = ", indexLeft)
-
-	// 二分查找最右一个元素
-	indexRight := binarySearchRightEdge(nums, target)
-	fmt.Println("数组中最右一个元素 6 的索引 = ", indexRight)
-}
