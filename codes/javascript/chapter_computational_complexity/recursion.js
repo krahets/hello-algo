620bfb1f1d566f770cf7e4e2520e2779519e17b5
gaofer <35647389+gaofer@users.noreply.github.com>
2023-09-02 16:09:13 +0800

    Iteration and recursion code in js/ts (#703)
    * feat(JS and TS):add the section of iteration and recursion
    
    * feat(JS and TS):add the section of iteration and recursion
    
    * feat(JS and TS):add the section of iteration and recursion

---
+++ codes/javascript/chapter_computational_complexity/recursion.js
@@ -0,0 +1,46 @@
+/**
+ * File: recursion.js
+ * Created Time: 2023-08-28
+ * Author: Gaofer Chou (gaofer-chou@qq.com)
+ */
+
+/* 递归 */
+function recur(n) {
+    // 终止条件
+    if (n === 1) return 1;
+    // 递：递归调用
+    const res = recur(n - 1);
+    // 归：返回结果
+    return n + res;
+}
+
+/* 尾递归 */
+function tailRecur(n, res) {
+    // 终止条件
+    if (n === 0) return res;
+    // 尾递归调用
+    return tailRecur(n - 1, res + n);
+}
+
+/* 斐波那契数列：递归 */
+function fib(n) {
+    // 终止条件 f(1) = 0, f(2) = 1
+    if (n === 1 || n === 2) return n - 1;
+    // 递归调用 f(n) = f(n-1) + f(n-2)
+    const res = fib(n - 1) + fib(n - 2);
+    // 返回结果 f(n)
+    return res;
+}
+
+/* Driver Code */
+const n = 5;
+let res;
+
+res = recur(n);
+console.log(`递归函数的求和结果 res = ${res}`);
+
+res = tailRecur(n, 0);
+console.log(`尾递归函数的求和结果 res = ${res}`);
+
+res = fib(n);
+console.log(`斐波那契数列的第 ${n} 项为 ${res}`);
