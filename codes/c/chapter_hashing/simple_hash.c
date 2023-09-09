b027731b62d41dea36fead72511f0683dc57964c
Gonglja <glj0@outlook.com>
2023-09-09 10:54:04 +0800

    feat(codes/c): Add `interation.c ` `recursion.c` `simple_hash.c` `binary_search_edge.c` `binary_search_insertion.c` in C codes.

---
+++ codes/c/chapter_hashing/simple_hash.c
@@ -0,0 +1,68 @@
+/**
+ * File: simple_hash.c
+ * Created Time: 2023-09-09
+ * Author: Gonglja (glj0@outlook.com)
+ */
+
+#include "../utils/common.h"
+
+/* 加法哈希 */
+int addHash(char *key) {
+    long long hash = 0;
+    const int MODULUS = 1000000007;
+    for (int i = 0; i < strlen(key); i++) {
+        hash = (hash + (unsigned char)key[i]) % MODULUS;
+    }
+    return (int)hash;
+}
+
+/* 乘法哈希 */
+int mulHash(char *key) {
+    long long hash = 0;
+    const int MODULUS = 1000000007;
+    for (int i = 0; i < strlen(key); i++) {
+        hash = (31 * hash + (unsigned char)key[i]) % MODULUS;
+    }
+    return (int)hash;
+}
+
+/* 异或哈希 */
+int xorHash(char *key) {
+    int hash = 0;
+    const int MODULUS = 1000000007;
+
+    for (int i = 0; i < strlen(key); i++) {
+        hash ^= (unsigned char)key[i];
+    }
+    return hash & MODULUS;
+}
+
+/* 旋转哈希 */
+int rotHash(char *key) {
+    long long hash = 0;
+    const int MODULUS = 1000000007;
+    for (int i = 0; i < strlen(key); i++) {
+        hash = ((hash << 4) ^ (hash >> 28) ^ (unsigned char)key[i]) % MODULUS;
+    }
+
+    return (int)hash;
+}
+
+/* Driver Code */
+int main() {
+    char *key = "Hello dsad3241241dsa算123法";
+
+    int hash = addHash(key);
+    printf("加法哈希值为 %d\n", hash);
+
+    hash = mulHash(key);
+    printf("乘法哈希值为 %d\n", hash);
+
+    hash = xorHash(key);
+    printf("异或哈希值为 %d\n", hash);
+
+    hash = rotHash(key);
+    printf("旋转哈希值为 %d\n", hash);
+
+    return 0;
+}
