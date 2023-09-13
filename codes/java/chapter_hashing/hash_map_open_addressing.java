3825046bc9813b3ae671fceb20b1a81e11027258
lyl625760 <lyl625760@163.com>
2023-09-13 11:54:52 +0800

    Update hash_map_open_addressing.java

--- codes/java/chapter_hashing/hash_map_open_addressing.java
+++ codes/java/chapter_hashing/hash_map_open_addressing.java
@@ -15,115 +15,118 @@ class HashMapOpenAddressing {
     private Pair[] buckets; // 桶数组
     private Pair removed; // 删除标记
 
-    /* 构造方法 */
+   /* 构造方法 */
     public HashMapOpenAddressing() {
         size = 0;
         capacity = 4;
-        loadThres = 2.0 / 3.0;
-        extendRatio = 2;
         buckets = new Pair[capacity];
-        removed = new Pair(-1, "-1");
     }
 
     /* 哈希函数 */
-    public int hashFunc(int key) {
+    private int hashFunc(int key) {
         return key % capacity;
     }
 
     /* 负载因子 */
-    public double loadFactor() {
+    private double loadFactor() {
         return (double) size / capacity;
+    }
+    /* 是否被占用 */
+    private boolean isOccupied(int index) {
+        return buckets[index] != null  && buckets[index] != removed;
+    }
+
+    /* 搜索 key 对应的桶索引 */
+    private int findBucket(int key) {
+        int index = hashFunc(key);
+        // 线性探测，当遇到空桶时跳出
+        while (isOccupied(index)) {
+            // 若遇到 key ，返回对应桶索引
+            if (buckets[index].key == key)
+                return index;
+            // 计算桶索引，越过尾部返回头部
+            index = (index + 1) % capacity;
+        }
+        return index;
     }
 
     /* 查询操作 */
     public String get(int key) {
-        int index = hashFunc(key);
-        // 线性探测，从 index 开始向后遍历
-        for (int i = 0; i < capacity; i++) {
-            // 计算桶索引，越过尾部返回头部
-            int j = (index + i) % capacity;
-            // 若遇到空桶，说明无此 key ，则返回 null
-            if (buckets[j] == null)
-                return null;
-            // 若遇到指定 key ，则返回对应 val
-            if (buckets[j].key == key && buckets[j] != removed)
-                return buckets[j].val;
-        }
+        int index = findBucket(key);
+        // 若找到桶索引，则返回对应 val
+        if (isOccupied(index))
+            return buckets[index].val;
+        // 若未找到桶索引，则返回 null
         return null;
     }
 
     /* 添加操作 */
     public void put(int key, String val) {
+        int index = findBucket(key);
+        // 若找到桶索引，则覆盖 val 并返回
+        if (isOccupied(index)) {
+            buckets[index].val = val;
+            return;
+        }
         // 当负载因子超过阈值时，执行扩容
         if (loadFactor() > loadThres) {
             extend();
+            index = findBucket(key);
         }
-        int index = hashFunc(key);
-        // 线性探测，从 index 开始向后遍历
-        for (int i = 0; i < capacity; i++) {
-            // 计算桶索引，越过尾部返回头部
-            int j = (index + i) % capacity;
-            // 若遇到空桶、或带有删除标记的桶，则将键值对放入该桶
-            if (buckets[j] == null || buckets[j] == removed) {
-                buckets[j] = new Pair(key, val);
-                size += 1;
-                return;
-            }
-            // 若遇到指定 key ，则更新对应 val
-            if (buckets[j].key == key) {
-                buckets[j].val = val;
-                return;
-            }
-        }
+
+        // 若未找到桶索引，则添加该键值对
+        buckets[index] = new Pair(key, val);
+        size++;
+
     }
 
     /* 删除操作 */
     public void remove(int key) {
-        int index = hashFunc(key);
-        // 线性探测，从 index 开始向后遍历
-        for (int i = 0; i < capacity; i++) {
-            // 计算桶索引，越过尾部返回头部
-            int j = (index + i) % capacity;
-            // 若遇到空桶，说明无此 key ，则直接返回
-            if (buckets[j] == null) {
-                return;
-            }
-            // 若遇到指定 key ，则标记删除并返回
-            if (buckets[j].key == key) {
-                buckets[j] = removed;
-                size -= 1;
+        int index = findBucket(key);
+        // 若找到桶索引，则标记删除它
+        if (isOccupied(index)) {
+            buckets[index] = removed;
+            size--;
+            // 移动,确保每个key的hash位置和实际位置之间都是连续占用的。
+            int j = (index + 1) % capacity;
+            while (isOccupied(j)) {
+                int k = hashFunc(buckets[j].key);
+                if ((index < j) && (k > j || k <= index) ||
+                        (index > j) && (k > j && k <= index)) {
+                    buckets[index] = buckets[j];
+                    buckets[j] = removed;
+                    index = j;
+                }
+                j = (j + 1) % capacity;
             }
         }
     }
 
     /* 扩容哈希表 */
-    public void extend() {
-        // 暂存原哈希表
-        Pair[] bucketsTmp = buckets;
+    private void extend() {
+        Pair[] oldBuckets = buckets;
         // 初始化扩容后的新哈希表
         capacity *= extendRatio;
         buckets = new Pair[capacity];
         size = 0;
         // 将键值对从原哈希表搬运至新哈希表
-        for (Pair pair : bucketsTmp) {
-            if (pair != null && pair != removed) {
+        for (Pair pair : oldBuckets) {
+            if (pair != null && pair != removed)
                 put(pair.key, pair.val);
-            }
         }
     }
 
     /* 打印哈希表 */
     public void print() {
         for (Pair pair : buckets) {
-            if (pair != null) {
+            if (pair != null)
                 System.out.println(pair.key + " -> " + pair.val);
-            } else {
+            else
                 System.out.println("null");
-            }
         }
     }
 }
-
+    
 public class hash_map_open_addressing {
     public static void main(String[] args) {
         /* 初始化哈希表 */
