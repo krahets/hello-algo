e964bb21c9fdee0c8baf976df3daac09d43eca81
Yudong Jin <krahets@163.com>
2023-09-21 04:36:25 -0500

    Update hash_map_open_addressing.java

--- codes/java/chapter_hashing/hash_map_open_addressing.java
+++ codes/java/chapter_hashing/hash_map_open_addressing.java
@@ -9,16 +9,15 @@ package chapter_hashing;
 /* 开放寻址哈希表 */
 class HashMapOpenAddressing {
     private int size; // 键值对数量
-    private int capacity; // 哈希表容量
-    private double loadThres; // 触发扩容的负载因子阈值
-    private int extendRatio; // 扩容倍数
+    private int capacity = 4; // 哈希表容量
+    private final double loadThres = 2.0 / 3; // 触发扩容的负载因子阈值
+    private final int extendRatio = 2; // 扩容倍数
     private Pair[] buckets; // 桶数组
-    private Pair removed; // 删除标记
+    private final Pair TOMBSTONE = new Pair(-1, "-1"); // 删除标记
 
-   /* 构造方法 */
+    /* 构造方法 */
     public HashMapOpenAddressing() {
         size = 0;
-        capacity = 4;
         buckets = new Pair[capacity];
     }
 
@@ -31,126 +30,129 @@ class HashMapOpenAddressing {
     private double loadFactor() {
         return (double) size / capacity;
     }
-    /* 是否被占用 */
-    private boolean isOccupied(int index) {
-        return buckets[index] != null  && buckets[index] != removed;
-    }
 
     /* 搜索 key 对应的桶索引 */
     private int findBucket(int key) {
         int index = hashFunc(key);
+        int firstTombstone = -1;
         // 线性探测，当遇到空桶时跳出
-        while (isOccupied(index)) {
+        while (buckets[index] != null) {
             // 若遇到 key ，返回对应桶索引
-            if (buckets[index].key == key)
-                return index;
+            if (buckets[index].key == key) {
+                // 若之前遇到了删除标记，则将键值对移动至该索引
+                if (firstTombstone != -1) {
+                    buckets[firstTombstone] = buckets[index];
+                    buckets[index] = TOMBSTONE;
+                    return firstTombstone; // 返回移动后的桶索引
+                }
+                return index; // 返回桶索引
+            }
+            // 记录遇到的首个删除标记
+            if (firstTombstone == -1 && buckets[index] == TOMBSTONE) {
+                firstTombstone = index;
+            }
             // 计算桶索引，越过尾部返回头部
             index = (index + 1) % capacity;
         }
-        return index;
+        // 若 key 不存在，则返回添加点的索引
+        return firstTombstone == -1 ? index : firstTombstone;
     }
 
     /* 查询操作 */
     public String get(int key) {
+        // 搜索 key 对应的桶索引
         int index = findBucket(key);
-        // 若找到桶索引，则返回对应 val
-        if (isOccupied(index))
+        // 若找到键值对，则返回对应 val
+        if (buckets[index] != null && buckets[index] != TOMBSTONE) {
             return buckets[index].val;
-        // 若未找到桶索引，则返回 null
+        }
+        // 若键值对不存在，则返回 null
         return null;
     }
 
     /* 添加操作 */
     public void put(int key, String val) {
-        int index = findBucket(key);
-        // 若找到桶索引，则覆盖 val 并返回
-        if (isOccupied(index)) {
-            buckets[index].val = val;
-            return;
-        }
         // 当负载因子超过阈值时，执行扩容
         if (loadFactor() > loadThres) {
             extend();
-            index = findBucket(key);
         }
-
-        // 若未找到桶索引，则添加该键值对
+        // 搜索 key 对应的桶索引
+        int index = findBucket(key);
+        // 若找到键值对，则覆盖 val 并返回
+        if (buckets[index] != null && buckets[index] != TOMBSTONE) {
+            buckets[index].val = val;
+            return;
+        }
+        // 若键值对不存在，则添加该键值对
         buckets[index] = new Pair(key, val);
         size++;
-
     }
 
     /* 删除操作 */
     public void remove(int key) {
+        // 搜索 key 对应的桶索引
         int index = findBucket(key);
-        // 若找到桶索引，则标记删除它
-        if (isOccupied(index)) {
-            buckets[index] = removed;
+        // 若找到键值对，则用删除标记覆盖它
+        if (buckets[index] != null && buckets[index] != TOMBSTONE) {
+            buckets[index] = TOMBSTONE;
             size--;
-            // 移动,确保每个key的hash位置和实际位置之间都是连续占用的。
-            int j = (index + 1) % capacity;
-            while (isOccupied(j)) {
-                int k = hashFunc(buckets[j].key);
-                if ((index < j) && (k > j || k <= index) ||
-                        (index > j) && (k > j && k <= index)) {
-                    buckets[index] = buckets[j];
-                    buckets[j] = removed;
-                    index = j;
-                }
-                j = (j + 1) % capacity;
-            }
         }
     }
 
     /* 扩容哈希表 */
     private void extend() {
-        Pair[] oldBuckets = buckets;
+        // 暂存原哈希表
+        Pair[] bucketsTmp = buckets;
         // 初始化扩容后的新哈希表
         capacity *= extendRatio;
         buckets = new Pair[capacity];
         size = 0;
         // 将键值对从原哈希表搬运至新哈希表
-        for (Pair pair : oldBuckets) {
-            if (pair != null && pair != removed)
+        for (Pair pair : bucketsTmp) {
+            if (pair != null && pair != TOMBSTONE) {
                 put(pair.key, pair.val);
+            }
         }
     }
 
     /* 打印哈希表 */
     public void print() {
         for (Pair pair : buckets) {
-            if (pair != null)
-                System.out.println(pair.key + " -> " + pair.val);
-            else
+            if (pair == null) {
                 System.out.println("null");
+            } else if (pair == TOMBSTONE) {
+                System.out.println("TOMBSTONE");
+            } else {
+                System.out.println(pair.key + " -> " + pair.val);
+            }
         }
     }
 }
-    
+
 public class hash_map_open_addressing {
     public static void main(String[] args) {
-        /* 初始化哈希表 */
-        HashMapOpenAddressing map = new HashMapOpenAddressing();
+        // 初始化哈希表
+        HashMapOpenAddressing hashmap = new HashMapOpenAddressing();
 
-        /* 添加操作 */
-        // 在哈希表中添加键值对 (key, value)
-        map.put(12836, "小哈");
-        map.put(15937, "小啰");
-        map.put(16750, "小算");
-        map.put(13276, "小法");
-        map.put(10583, "小鸭");
+        // 添加操作
+        // 在哈希表中添加键值对 (key, val)
+        hashmap.put(12836, "小哈");
+        hashmap.put(15937, "小啰");
+        hashmap.put(16750, "小算");
+        hashmap.put(13276, "小法");
+        hashmap.put(10583, "小鸭");
         System.out.println("\n添加完成后，哈希表为\nKey -> Value");
-        map.print();
+        hashmap.print();
 
-        /* 查询操作 */
-        // 向哈希表输入键 key ，得到值 value
-        String name = map.get(13276);
+        // 查询操作
+        // 向哈希表输入键 key ，得到值 val
+        String name = hashmap.get(13276);
         System.out.println("\n输入学号 13276 ，查询到姓名 " + name);
 
-        /* 删除操作 */
-        // 在哈希表中删除键值对 (key, value)
-        map.remove(16750);
+        // 删除操作
+        // 在哈希表中删除键值对 (key, val)
+        hashmap.remove(16750);
         System.out.println("\n删除 16750 后，哈希表为\nKey -> Value");
-        map.print();
+        hashmap.print();
     }
 }
