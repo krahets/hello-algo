b18e809aec343c45af57017a29dde6100a7604ea
jmin <1256527506@qq.com>
2023-09-23 23:57:21 +0800

    style: update code order th same with java

--- codes/typescript/chapter_computational_complexity/recursion.ts
+++ codes/typescript/chapter_computational_complexity/recursion.ts
@@ -14,24 +14,6 @@ function recur(n: number): number {
     return n + res;
 }
 
-/* 尾递归 */
-function tailRecur(n: number, res: number): number {
-    // 终止条件
-    if (n === 0) return res;
-    // 尾递归调用
-    return tailRecur(n - 1, res + n);
-}
-
-/* 斐波那契数列：递归 */
-function fib(n: number): number {
-    // 终止条件 f(1) = 0, f(2) = 1
-    if (n === 1 || n === 2) return n - 1;
-    // 递归调用 f(n) = f(n-1) + f(n-2)
-    const res = fib(n - 1) + fib(n - 2);
-    // 返回结果 f(n)
-    return res;
-}
-
 /* 递归转化为迭代 */
 function forLoopRecur(n: number): number {
     // 使用一个显式的栈来模拟系统调用栈 
@@ -50,6 +32,25 @@ function forLoopRecur(n: number): number {
     // res = 1+2+3+...+n
     return res;
 }
+
+/* 尾递归 */
+function tailRecur(n: number, res: number): number {
+    // 终止条件
+    if (n === 0) return res;
+    // 尾递归调用
+    return tailRecur(n - 1, res + n);
+}
+
+/* 斐波那契数列：递归 */
+function fib(n: number): number {
+    // 终止条件 f(1) = 0, f(2) = 1
+    if (n === 1 || n === 2) return n - 1;
+    // 递归调用 f(n) = f(n-1) + f(n-2)
+    const res = fib(n - 1) + fib(n - 2);
+    // 返回结果 f(n)
+    return res;
+}
+
 /* Driver Code */
 const n = 5;
 let res: number;
@@ -57,13 +58,13 @@ let res: number;
 res = recur(n);
 console.log(`递归函数的求和结果 res = ${res}`);
 
+res = forLoopRecur(n);
+console.log(`递归转化为迭代的求和结果 res = ${res}`);
+
 res = tailRecur(n, 0);
 console.log(`尾递归函数的求和结果 res = ${res}`);
 
 res = fib(n);
 console.log(`斐波那契数列的第 ${n} 项为 ${res}`);
-
-res = forLoopRecur(n);
-console.log(`递归转化为迭代的求和结果 res = ${res}`);
 
 export {};
