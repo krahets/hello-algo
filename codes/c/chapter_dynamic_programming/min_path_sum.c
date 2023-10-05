ea922f909a9356bd29cc4e3cff0cadda4901014e
王作勋 <59754483+Zuoxun@users.noreply.github.com>
2023-10-06 00:47:17 +0800

    Update min_path_sum.c

--- codes/c/chapter_dynamic_programming/min_path_sum.c
+++ codes/c/chapter_dynamic_programming/min_path_sum.c
@@ -6,15 +6,13 @@
 
 #include "../utils/common.h"
 
-int n, m;
-
 /* 求最小值 */
 int min(int a, int b) {
     return a < b ? a : b;
 }
 
 /* 最小路径和：暴力搜索 */
-int minPathSumDFS(int grid[][m], int i, int j) {
+int minPathSumDFS(int gridCols, int grid[][gridCols], int i, int j) {
     // 若为左上角单元格，则终止搜索
     if (i == 0 && j == 0) {
         return grid[0][0];
@@ -24,14 +22,14 @@ int minPathSumDFS(int grid[][m], int i, int j) {
         return INT_MAX;
     }
     // 计算从左上角到 (i-1, j) 和 (i, j-1) 的最小路径代价
-    int left = minPathSumDFS(grid, i - 1, j);
-    int up = minPathSumDFS(grid, i, j - 1);
+    int left = minPathSumDFS(gridCols, grid, i - 1, j);
+    int up = minPathSumDFS(gridCols, grid, i, j - 1);
     // 返回从左上角到 (i, j) 的最小路径代价
     return min(left, up) != INT_MAX ? min(left, up) + grid[i][j] : INT_MAX;
 }
 
 /* 最小路径和：记忆化搜索 */
-int minPathSumDFSMem(int grid[][m], int mem[][m], int i, int j) {
+int minPathSumDFSMem(int gridCols, int grid[][gridCols], int mem[][gridCols], int i, int j) {
     // 若为左上角单元格，则终止搜索
     if (i == 0 && j == 0) {
         return grid[0][0];
@@ -45,15 +43,15 @@ int minPathSumDFSMem(int grid[][m], int mem[][m], int i, int j) {
         return mem[i][j];
     }
     // 左边和上边单元格的最小路径代价
-    int left = minPathSumDFSMem(grid, mem, i - 1, j);
-    int up = minPathSumDFSMem(grid, mem, i, j - 1);
+    int left = minPathSumDFSMem(gridCols, grid, mem, i - 1, j);
+    int up = minPathSumDFSMem(gridCols, grid, mem, i, j - 1);
     // 记录并返回左上角到 (i, j) 的最小路径代价
     mem[i][j] = min(left, up) != INT_MAX ? min(left, up) + grid[i][j] : INT_MAX;
     return mem[i][j];
 }
 
 /* 最小路径和：动态规划 */
-int minPathSumDP(int grid[][m], int n, int m) {
+int minPathSumDP(int gridCols, int grid[][gridCols], int n, int m) {
     // 初始化 dp 表
     int dp[n][m];
     dp[0][0] = grid[0][0];
@@ -75,7 +73,7 @@ int minPathSumDP(int grid[][m], int n, int m) {
 }
 
 /* 最小路径和：空间优化后的动态规划 */
-int minPathSumDPComp(int grid[][m], int n, int m) {
+int minPathSumDPComp(int gridCols, int grid[][gridCols], int n, int m) {
     // 初始化 dp 表
     int dp[m];
     // 状态转移：首行
@@ -98,24 +96,24 @@ int minPathSumDPComp(int grid[][m], int n, int m) {
 /* Driver Code */
 int main() {
     int grid[][4] = {{1, 3, 1, 5}, {2, 2, 4, 2}, {5, 3, 2, 1}, {4, 3, 5, 2}};
-    n = sizeof(grid) / sizeof(grid[0]), m = sizeof(grid[0]) / sizeof(grid[0][0]);
+    int n = sizeof(grid) / sizeof(grid[0]), m = sizeof(grid[0]) / sizeof(grid[0][0]);
 
     // 暴力搜索
-    int res = minPathSumDFS(grid, n - 1, m - 1);
+    int res = minPathSumDFS(m, grid, n - 1, m - 1);
     printf("从左上角到右下角的最小路径和为 %d\n", res);
 
     // 记忆化搜索
     int mem[n][m];
     memset(mem, -1, sizeof(mem));
-    res = minPathSumDFSMem(grid, mem, n - 1, m - 1);
+    res = minPathSumDFSMem(m, grid, mem, n - 1, m - 1);
     printf("从左上角到右下角的最小路径和为 %d\n", res);
 
     // 动态规划
-    res = minPathSumDP(grid, n, m);
+    res = minPathSumDP(m, grid, n, m);
     printf("从左上角到右下角的最小路径和为 %d\n", res);
 
     // 空间优化后的动态规划
-    res = minPathSumDPComp(grid, n, m);
+    res = minPathSumDPComp(m, grid, n, m);
     printf("从左上角到右下角的最小路径和为 %d\n", res);
 
     return 0;
