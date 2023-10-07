8966ee0127febef60a57a3956a47b9634376a24d
王作勋 <59754483+Zuoxun@users.noreply.github.com>
2023-10-07 21:42:48 +0800

    Update binary_search_recur.c

--- codes/c/chapter_divide_and_conquer/binary_search_recur.c
+++ codes/c/chapter_divide_and_conquer/binary_search_recur.c
@@ -7,18 +7,17 @@
 #include "../utils/common.h"
 
 /* 二分查找：问题 f(i, j) */
-int dfs(vector *nums, int target, int i, int j) {
+int dfs(int nums[], int target, int i, int j) {
     // 若区间为空，代表无目标元素，则返回 -1
     if (i > j) {
         return -1;
     }
     // 计算中点索引 m
     int m = (i + j) / 2;
-    int *elem = nums->data[m];
-    if (*elem < target) {
+    if (nums[m] < target) {
         // 递归子问题 f(m+1, j)
         return dfs(nums, target, m + 1, j);
-    } else if (*elem > target) {
+    } else if (nums[m] > target) {
         // 递归子问题 f(i, m-1)
         return dfs(nums, target, i, m - 1);
     } else {
@@ -28,8 +27,8 @@ int dfs(vector *nums, int target, int i, int j) {
 }
 
 /* 二分查找 */
-int binarySearch(vector *nums, int target) {
-    int n = nums->size;
+int binarySearch(int nums[], int target, int numsSize) {
+    int n = numsSize;
     // 求解问题 f(0, n-1)
     return dfs(nums, target, 0, n - 1);
 }
@@ -38,16 +37,11 @@ int binarySearch(vector *nums, int target) {
 int main() {
     int target = 6;
     int nums[] = {1, 3, 6, 8, 12, 15, 23, 26, 31, 35};
-    vector *iNums = newVector(); // int
-    for (int i = 0; i < sizeof(nums) / sizeof(nums[0]); i++) {
-        vectorPushback(iNums, &nums[i], sizeof(int));
-    }
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
 
     // 二分查找（双闭区间）
-    int index = binarySearch(iNums, target);
+    int index = binarySearch(nums, target, numsSize);
     printf("目标元素 6 的索引 = %d\n", index);
-
-    // 释放内存
-    delVector(iNums);
+    
     return 0;
 }
