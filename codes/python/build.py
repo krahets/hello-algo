d27890d98ddedfd86f23c9aef12ab422b413f1a2
krahets <krahets@163.com>
2023-10-23 16:06:33 +0800

    Fix automating build workflow for Python

--- codes/python/build.py
+++ codes/python/build.py
@@ -1,17 +1,20 @@
 import glob
 import py_compile as pyc
 
-src_paths = sorted(glob.glob("codes/python/**/*.py"))
-num_src = len(src_paths)
-num_src_error = 0
+if __name__ == "__main__":
+    # find source code files
+    src_paths = sorted(glob.glob("codes/python/**/*.py"))
+    num_src = len(src_paths)
+    num_src_error = 0
 
-for src_path in src_paths:
-    try:
-        pyc.compile(src_path, doraise=True)
-    except pyc.PyCompileError as e:
-        num_src_error += 1
-        print(e)
+    # compile python code
+    for src_path in src_paths:
+        try:
+            pyc.compile(src_path, doraise=True)
+        except pyc.PyCompileError as e:
+            num_src_error += 1
+            print(e)
 
-print(f"===== Build Summary =====")
-print(f"Total: {num_src}")
-print(f"Error: {num_src - num_src_error}")
+    print(f"===== Build Complete =====")
+    print(f"Total: {num_src}")
+    print(f"Error: {num_src_error}")
