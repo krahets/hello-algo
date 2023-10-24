36de7aa4e80cfbab3ffd0c8720bb846f928317bf
Yudong Jin <krahets@163.com>
2023-10-24 23:42:34 +0800

    Update hash_map_open_addressing.js

--- codes/javascript/chapter_hashing/hash_map_open_addressing.js
+++ codes/javascript/chapter_hashing/hash_map_open_addressing.js
@@ -4,7 +4,7 @@
  * Author: yuan0221 (yl1452491917@gmail.com), Krahets (krahets@163.com)
  */
 
-/* 键值对 */
+/* 键值对 Number -> String */
 class Pair {
     constructor(key, val) {
         this.key = key;
@@ -14,38 +14,45 @@ class Pair {
 
 /* 开放寻址哈希表 */
 class HashMapOpenAddressing {
+    #size; // 键值对数量
+    #capacity; // 哈希表容量
+    #loadThres; // 触发扩容的负载因子阈值
+    #extendRatio; // 扩容倍数
+    #buckets; // 桶数组
+    #TOMBSTONE; // 删除标记
+
     /* 构造方法 */
     constructor() {
-        this.size = 0; // 键值对数量
-        this.capacity = 4; // 哈希表容量
-        this.loadThres = 2.0 / 3.0; // 触发扩容的负载因子阈值
-        this.extendRatio = 2; // 扩容倍数
-        this.buckets = Array(this.capacity).fill(null); // 桶数组
-        this.TOMBSTONE = new Pair(-1, '-1'); // 删除标记
+        this.#size = 0; // 键值对数量
+        this.#capacity = 4; // 哈希表容量
+        this.#loadThres = 2.0 / 3.0; // 触发扩容的负载因子阈值
+        this.#extendRatio = 2; // 扩容倍数
+        this.#buckets = Array(this.#capacity).fill(null); // 桶数组
+        this.#TOMBSTONE = new Pair(-1, '-1'); // 删除标记
     }
 
     /* 哈希函数 */
-    hashFunc(key) {
-        return key % this.capacity;
+    #hashFunc(key) {
+        return key % this.#capacity;
     }
 
     /* 负载因子 */
     loadFactor() {
-        return this.size / this.capacity;
+        return this.#size / this.#capacity;
     }
 
     /* 搜索 key 对应的桶索引 */
-    findBucket(key) {
-        let index = this.hashFunc(key);
+    #findBucket(key) {
+        let index = this.#hashFunc(key);
         let firstTombstone = -1;
         // 线性探测，当遇到空桶时跳出
-        while (this.buckets[index] !== null) {
+        while (this.#buckets[index] !== null) {
             // 若遇到 key ，返回对应桶索引
-            if (this.buckets[index].key === key) {
+            if (this.#buckets[index].key === key) {
                 // 若之前遇到了删除标记，则将键值对移动至该索引
                 if (firstTombstone !== -1) {
-                    this.buckets[firstTombstone] = this.buckets[index];
-                    this.buckets[index] = this.TOMBSTONE;
+                    this.#buckets[firstTombstone] = this.#buckets[index];
+                    this.#buckets[index] = this.#TOMBSTONE;
                     return firstTombstone; // 返回移动后的桶索引
                 }
                 return index; // 返回桶索引
@@ -53,12 +60,12 @@ class HashMapOpenAddressing {
             // 记录遇到的首个删除标记
             if (
                 firstTombstone === -1 &&
-                this.buckets[index] === this.TOMBSTONE
+                this.#buckets[index] === this.#TOMBSTONE
             ) {
                 firstTombstone = index;
             }
             // 计算桶索引，越过尾部返回头部
-            index = (index + 1) % this.capacity;
+            index = (index + 1) % this.#capacity;
         }
         // 若 key 不存在，则返回添加点的索引
         return firstTombstone === -1 ? index : firstTombstone;
@@ -67,13 +74,13 @@ class HashMapOpenAddressing {
     /* 查询操作 */
     get(key) {
         // 搜索 key 对应的桶索引
-        const index = this.findBucket(key);
+        const index = this.#findBucket(key);
         // 若找到键值对，则返回对应 val
         if (
-            this.buckets[index] !== null &&
-            this.buckets[index] !== this.TOMBSTONE
+            this.#buckets[index] !== null &&
+            this.#buckets[index] !== this.#TOMBSTONE
         ) {
-            return this.buckets[index].val;
+            return this.#buckets[index].val;
         }
         // 若键值对不存在，则返回 null
         return null;
@@ -82,49 +89,49 @@ class HashMapOpenAddressing {
     /* 添加操作 */
     put(key, val) {
         // 当负载因子超过阈值时，执行扩容
-        if (this.loadFactor() > this.loadThres) {
+        if (this.loadFactor() > this.#loadThres) {
             this.extend();
         }
         // 搜索 key 对应的桶索引
-        const index = this.findBucket(key);
+        const index = this.#findBucket(key);
         // 若找到键值对，则覆盖 val 并返回
         if (
-            this.buckets[index] !== null &&
-            this.buckets[index] !== this.TOMBSTONE
+            this.#buckets[index] !== null &&
+            this.#buckets[index] !== this.#TOMBSTONE
         ) {
-            this.buckets[index].val = val;
+            this.#buckets[index].val = val;
             return;
         }
         // 若键值对不存在，则添加该键值对
-        this.buckets[index] = new Pair(key, val);
-        this.size++;
+        this.#buckets[index] = new Pair(key, val);
+        this.#size++;
     }
 
     /* 删除操作 */
     remove(key) {
         // 搜索 key 对应的桶索引
-        const index = this.findBucket(key);
+        const index = this.#findBucket(key);
         // 若找到键值对，则用删除标记覆盖它
         if (
-            this.buckets[index] !== null &&
-            this.buckets[index] !== this.TOMBSTONE
+            this.#buckets[index] !== null &&
+            this.#buckets[index] !== this.#TOMBSTONE
         ) {
-            this.buckets[index] = this.TOMBSTONE;
-            this.size--;
+            this.#buckets[index] = this.#TOMBSTONE;
+            this.#size--;
         }
     }
 
     /* 扩容哈希表 */
     extend() {
         // 暂存原哈希表
-        const bucketsTmp = this.buckets;
+        const bucketsTmp = this.#buckets;
         // 初始化扩容后的新哈希表
-        this.capacity *= this.extendRatio;
-        this.buckets = Array(this.capacity).fill(null);
-        this.size = 0;
+        this.#capacity *= this.#extendRatio;
+        this.#buckets = Array(this.#capacity).fill(null);
+        this.#size = 0;
         // 将键值对从原哈希表搬运至新哈希表
         for (const pair of bucketsTmp) {
-            if (pair !== null && pair !== this.TOMBSTONE) {
+            if (pair !== null && pair !== this.#TOMBSTONE) {
                 this.put(pair.key, pair.val);
             }
         }
@@ -132,10 +139,10 @@ class HashMapOpenAddressing {
 
     /* 打印哈希表 */
     print() {
-        for (const pair of this.buckets) {
+        for (const pair of this.#buckets) {
             if (pair === null) {
                 console.log('null');
-            } else if (pair === this.TOMBSTONE) {
+            } else if (pair === this.#TOMBSTONE) {
                 console.log('TOMBSTONE');
             } else {
                 console.log(pair.key + ' -> ' + pair.val);
