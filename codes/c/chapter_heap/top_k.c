1a9fcb4fa9c061e1d372061bd1025f3bf024ee06
lucas <superrat6@gmail.com>
2023-10-26 01:14:22 +0800

    chore: update top_k.c
    使用最小堆实现

--- codes/c/chapter_heap/top_k.c
+++ codes/c/chapter_heap/top_k.c
@@ -1,44 +1,61 @@
 /**
- * File: top_k.cpp
+ * File: top_k.c
  * Created Time: 2023-10-10
  * Author: lucas (superrat6@gmail.com)
  */
 
 #include "../utils/common.h"
 
-/* 用于最小堆的比较函数 */
-int compare(const void *a, const void *b) {
-    return (*(int *)a - *(int *)b);
+#define HEAP_MAX_SIZE 100
+
+typedef struct {
+    int size;
+    int data[HEAP_MAX_SIZE];
+} MinHeap;
+
+/* 构造最小堆 */
+MinHeap* createHeap() {
+    MinHeap* heap = (MinHeap*)malloc(sizeof(MinHeap));
+    heap->size = 0;
+    return heap;
+}
+
+/* 将数据放入堆 */
+void pushHeap(MinHeap* heap, int value) {
+    if (heap->size == HEAP_MAX_SIZE) {
+        if (heap->data[0] < value) {
+            heap->data[0] = value;
+        }
+        return;
+    }
+
+    int i = heap->size++;
+    while (i > 0) {
+        int parent = (i - 1) / 2;
+        if (heap->data[parent] <= value) break;
+        heap->data[i] = heap->data[parent];
+        i = parent;
+    }
+    heap->data[i] = value;
 }
 
 /* 基于堆查找数组中最大的 k 个元素 */
-int *topKHeap(int *nums, int size, int k) {
-    // 创建一个大小为 k 的最小堆    
-    int *heap = (int *)malloc(k * sizeof(int));
+MinHeap *topKHeap(int *nums, int size, int k) {
+    // 创建一个最小堆    
+    MinHeap *heap = createHeap();
     for (int i = 0; i < k; i++) {
-        heap[i] = nums[i];
+        pushHeap(heap, nums[i]);
     }
-    qsort(heap, k, sizeof(int), compare);
 
     // 从第 k+1 个元素开始，保持堆的长度为 k
     for (int i = k; i < size; i++) {
         // 若当前元素大于堆顶元素，则将堆顶元素替换为当前元素，并重新调整堆
-        if (nums[i] > heap[0]) {
-            heap[0] = nums[i];
-            qsort(heap, k, sizeof(int), compare);
+        if (nums[i] > heap->data[0]) {
+            heap->data[0] = nums[i];
         }
     }
 
-    int *result = (int *)malloc(k * sizeof(int));
-    // 将最大的 k 个元素复制到结果数组中
-    for (int i = 0; i < k; i++) {
-        result[i] = heap[i];
-    }
-
-    // 释放堆内存
-    free(heap);
-
-    return result;
+    return heap;
 }
 
 /* Driver Code */
@@ -47,10 +64,10 @@ int main() {
     int size = sizeof(nums) / sizeof(nums[0]);
     int k = 3;
 
-    int *result = topKHeap(nums, size, k);
+    MinHeap *result = topKHeap(nums, size, k);
 
     printf("最大的 %d 个元素为: ", k);
-    printHeap(result, k);
+    printHeap(result->data, k);
 
     free(result);
 
