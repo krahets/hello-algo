7f10d5efefae965990074f42f3ea46cc951dc338
krahets <krahets@163.com>
2023-10-25 23:02:27 +0800

    Re-implement merge sort function.

--- codes/typescript/chapter_sorting/merge_sort.ts
+++ codes/typescript/chapter_sorting/merge_sort.ts
@@ -5,32 +5,32 @@
  */
 
 /* 合并左子数组和右子数组 */
-// 左子数组区间 [left, mid]
-// 右子数组区间 [mid + 1, right]
 function merge(nums: number[], left: number, mid: number, right: number): void {
-    // 初始化辅助数组
-    let tmp = nums.slice(left, right + 1);
-    // 左子数组的起始索引和结束索引
-    let leftStart = left - left,
-        leftEnd = mid - left;
-    // 右子数组的起始索引和结束索引
-    let rightStart = mid + 1 - left,
-        rightEnd = right - left;
-    // i, j 分别指向左子数组、右子数组的首元素
-    let i = leftStart,
-        j = rightStart;
-    // 通过覆盖原数组 nums 来合并左子数组和右子数组
-    for (let k = left; k <= right; k++) {
-        if (i > leftEnd) {
-            // 若“左子数组已全部合并完”，则选取右子数组元素，并且 j++
-            nums[k] = tmp[j++];
-            // 否则，若“右子数组已全部合并完”或“左子数组元素 <= 右子数组元素”，则选取左子数组元素，并且 i++
-        } else if (j > rightEnd || tmp[i] <= tmp[j]) {
-            nums[k] = tmp[i++];
-            // 否则，若“左右子数组都未全部合并完”且“左子数组元素 > 右子数组元素”，则选取右子数组元素，并且 j++
+    // 左子数组区间 [left, mid], 右子数组区间 [mid+1, right]
+    // 创建一个临时数组 tmp ，用于存放合并后的结果
+    const tmp = new Array(right - left + 1);
+    // 初始化左子数组和右子数组的起始索引
+    let i = left,
+        j = mid + 1,
+        k = 0;
+    // 当左右子数组都还有元素时，比较并将较小的元素复制到临时数组中
+    while (i <= mid && j <= right) {
+        if (nums[i] <= nums[j]) {
+            tmp[k++] = nums[i++];
         } else {
-            nums[k] = tmp[j++];
+            tmp[k++] = nums[j++];
         }
+    }
+    // 将左子数组和右子数组的剩余元素复制到临时数组中
+    while (i <= mid) {
+        tmp[k++] = nums[i++];
+    }
+    while (j <= right) {
+        tmp[k++] = nums[j++];
+    }
+    // 将临时数组 tmp 中的元素复制回原数组 nums 的对应区间
+    for (k = 0; k < tmp.length; k++) {
+        nums[left + k] = tmp[k];
     }
 }
 
