db41463da8cdbb2f75e95c6bcb9e1a562f4b8cde
krahets <krahets@163.com>
2023-12-22 00:01:12 +0800

    deploy

---
+++ en/javascripts/mathjax.js
@@ -0,0 +1,17 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex",
+    enableMenu: false,
+  },
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise();
+});
