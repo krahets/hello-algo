4ff5896ee99a72095cf48c350d39c577e3e2c8a5
Johnson <577899820@qq.com>
2024-01-23 21:25:06 +0800

    feat(add new chapter in Kotlin)
        add new chapter 'chapter_computational_complexity',
        written in Kotlin

---
+++ codes/kotlin/chapter_computational_complexity/recursion.kt
@@ -0,0 +1,73 @@
+package chapter_computational_complexity
+
+import java.util.*
+
+class recursion {
+    /* 递归 */
+    fun recur(n: Int): Int {
+        // 终止条件
+        if (n == 1)
+            return 1
+        // 递: 递归调用
+        val res = recur(n - 1)
+        // 归: 返回结果
+        return n + res
+    }
+
+    /* 使用迭代模拟递归 */
+    fun forLoopRecur(n: Int): Int {
+        // 使用一个显式的栈来模拟系统调用栈{
+        val stack = Stack<Int>()
+        var res = 0
+        // 递: 递归调用
+        for (i in n downTo 0) {
+            stack.push(i)
+        }
+        // 归: 返回结果
+        while (stack.isNotEmpty()) {
+            // 通过“出栈操作”模拟“归”
+            res += stack.pop()
+        }
+        // res = 1+2+3+...+n
+        return res
+    }
+
+    /* Kotlin tailrec 关键词使函数实现尾递归优化  */
+    tailrec fun tailRecur(n: Int, res: Int): Int {
+        // 终止条件
+        if (n == 0)
+            return res
+        // 尾递归调用
+        return tailRecur(n - 1, res + n)
+    }
+
+    /* 斐波那契数列：递归 */
+    fun fib(n: Int): Int {
+        // 终止条件 f(1) = 0, f(2) = 1
+        if (n == 1 || n == 2)
+            return n - 1
+        // 递归调用 f(n) = f(n-1) + f(n-2)
+        val res = fib(n - 1) + fib(n - 2)
+        // 返回结果 f(n)
+        return res
+    }
+}
+
+/* Driver Code */
+fun main() {
+    val n = 5
+    var res: Int
+    val rc = recursion()
+
+    res = rc.recur(n)
+    println("\n递归函数的求和结果 res = $res")
+
+    res = rc.forLoopRecur(n)
+    println("\n使用迭代模拟递归求和结果 res = $res")
+
+    res = rc.tailRecur(n, 0)
+    println("\n尾递归函数的求和结果 res = $res")
+
+    res = rc.fib(n)
+    println("\n斐波那契数列的第 $n 项为 $res")
+}
