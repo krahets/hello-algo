19cb81f23fe19bfd690fc51cfa0179da7c3d6a5d
curtishd <1023632660@qq.com>
2024-03-03 17:56:05 +0800

    style(kotlin): reformatted the codes.

--- codes/kotlin/chapter_graph/graph_adjacency_matrix.kt
+++ codes/kotlin/chapter_graph/graph_adjacency_matrix.kt
@@ -9,9 +9,9 @@ package chapter_graph
 import utils.printMatrix
 
 /* 基于邻接矩阵实现的无向图类 */
-class GraphAdjMat(vertices:IntArray,edges:Array<IntArray>) {
-    val vertices:MutableList<Int> = ArrayList() // 顶点列表，元素代表“顶点值”，索引代表“顶点索引”
-    val adjMat:MutableList<MutableList<Int>> = ArrayList() // 邻接矩阵，行列索引对应“顶点索引”
+class GraphAdjMat(vertices: IntArray, edges: Array<IntArray>) {
+    val vertices: MutableList<Int> = ArrayList() // 顶点列表，元素代表“顶点值”，索引代表“顶点索引”
+    val adjMat: MutableList<MutableList<Int>> = ArrayList() // 邻接矩阵，行列索引对应“顶点索引”
 
     /* 构造函数 */
     init {
@@ -22,23 +22,23 @@ class GraphAdjMat(vertices:IntArray,edges:Array<IntArray>) {
         // 添加边
         // 请注意，edges 元素代表顶点索引，即对应 vertices 元素索引
         for (edge in edges) {
-            addEdge(edge[0],edge[1])
+            addEdge(edge[0], edge[1])
         }
     }
 
     /* 获取顶点数量 */
-    fun size():Int{
+    fun size(): Int {
         return vertices.size
     }
 
     /* 添加顶点 */
-    fun addVertex(value:Int){
-        val n=size()
+    fun addVertex(value: Int) {
+        val n = size()
         // 向顶点列表中添加新顶点的值
         vertices.add(value)
         // 在邻接矩阵中添加一行
-        val newRow:MutableList<Int> = mutableListOf()
-        for (j in 0..<n){
+        val newRow: MutableList<Int> = mutableListOf()
+        for (j in 0..<n) {
             newRow.add(0)
         }
         adjMat.add(newRow)
@@ -63,7 +63,7 @@ class GraphAdjMat(vertices:IntArray,edges:Array<IntArray>) {
 
     /* 添加边 */
     // 参数 i, j 对应 vertices 元素索引
-    fun addEdge(i:Int,j:Int){
+    fun addEdge(i: Int, j: Int) {
         // 索引越界与相等处理
         if (i < 0 || j < 0 || i >= size() || j >= size() || i == j) throw java.lang.IndexOutOfBoundsException()
         // 在无向图中，邻接矩阵关于主对角线对称，即满足 (i, j) == (j, i)
@@ -73,7 +73,7 @@ class GraphAdjMat(vertices:IntArray,edges:Array<IntArray>) {
 
     /* 删除边 */
     // 参数 i, j 对应 vertices 元素索引
-    fun removeEdge(i:Int,j: Int){
+    fun removeEdge(i: Int, j: Int) {
         // 索引越界与相等处理
         if (i < 0 || j < 0 || i >= size() || j >= size() || i == j) throw java.lang.IndexOutOfBoundsException()
         adjMat[i][j] = 0;
@@ -81,7 +81,7 @@ class GraphAdjMat(vertices:IntArray,edges:Array<IntArray>) {
     }
 
     /* 打印邻接矩阵 */
-    fun print(){
+    fun print() {
         print("顶点列表 = ")
         println(vertices);
         println("邻接矩阵 =");
