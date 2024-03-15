1c09c86338606de8e7b033a2f600021da78c821d
krahets <krahets@163.com>
2024-03-15 17:01:46 +0800

    Remove the empty space

--- codes/swift/chapter_sorting/bubble_sort.swift
+++ codes/swift/chapter_sorting/bubble_sort.swift
@@ -8,7 +8,7 @@
 func bubbleSort(nums: inout [Int]) {
     // 外循环：未排序区间为 [0, i]
     for i in stride(from: nums.count - 1, to: 0, by: -1) {
-        // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端 
+        // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
         for j in stride(from: 0, to: i, by: 1) {
             if nums[j] > nums[j + 1] {
                 // 交换 nums[j] 与 nums[j + 1]
