016d65f29a7824ab70bd0a6d91bf7d42cc79f6c0
curtishd <1023632660@qq.com>
2024-03-16 08:28:47 +0800

    style(kotlin): fix some indent.

--- codes/kotlin/chapter_sorting/QuickSort.kt
+++ codes/kotlin/chapter_sorting/QuickSort.kt
@@ -18,9 +18,7 @@ fun partition(nums: IntArray, left: Int, right: Int): Int {
     var j = right
     while (i < j) {
         while (i < j && nums[j] >= nums[left]) j-- // 从右向左找首个小于基准数的元素
-
         while (i < j && nums[i] <= nums[left]) i++ // 从左向右找首个大于基准数的元素
-
         swap(nums, i, j) // 交换这两个元素
     }
     nums[i] = nums[left].also { nums[left] = nums[i] } // 将基准数交换至两子数组的分界线
