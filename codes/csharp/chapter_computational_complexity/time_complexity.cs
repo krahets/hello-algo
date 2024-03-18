7f43f92ae9e88ec2292967d79c6e2db662947cfa
Yudong Jin <krahets@163.com>
2024-03-18 13:34:02 +0800

    Bug fixes and improvements (#1152)
    * Update avl_tree.md
    
    * Remove the empty space
    
    * Simplify the heading of the paperbook chapter
    
    * Update hash_map_open_addressing.go to the latest version
    
    * Improvements

--- codes/csharp/chapter_computational_complexity/time_complexity.cs
+++ codes/csharp/chapter_computational_complexity/time_complexity.cs
@@ -85,7 +85,7 @@ public class time_complexity {
         int count = 0;  // 计数器
         // 外循环：未排序区间为 [0, i]
         for (int i = nums.Length - 1; i > 0; i--) {
-            // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端 
+            // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
             for (int j = 0; j < i; j++) {
                 if (nums[j] > nums[j + 1]) {
                     // 交换 nums[j] 与 nums[j + 1]
--- codes/swift/chapter_computational_complexity/time_complexity.swift
+++ codes/swift/chapter_computational_complexity/time_complexity.swift
@@ -50,7 +50,7 @@ func bubbleSort(nums: inout [Int]) -> Int {
     var count = 0 // 计数器
     // 外循环：未排序区间为 [0, i]
     for i in stride(from: nums.count - 1, to: 0, by: -1) {
-        // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端 
+        // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
         for j in 0 ..< i {
             if nums[j] > nums[j + 1] {
                 // 交换 nums[j] 与 nums[j + 1]
