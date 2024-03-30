193d933620e242c9e021b77f176f8a7cbe80a06a
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2024-03-30 22:34:56 +0700

    feat: add ruby code - chapter computational complexity

--- codes/ruby/utils/print_util.rb
+++ codes/ruby/utils/print_util.rb
@@ -13,3 +13,46 @@ def print_linked_list(head)
   end
   puts "#{list.join(" -> ")}"
 end
+
+class Trunk
+  attr_accessor :prev, :str
+
+  def initialize(prev, str)
+    @prev = prev
+    @str = str
+  end
+end
+
+def show_trunk(p)
+  return if p.nil?
+
+  show_trunk(p.prev)
+  print p.str
+end
+
+### 打印二叉树 ###
+# This tree printer is borrowed from TECHIE DELIGHT
+# https://www.techiedelight.com/c-program-print-binary-tree/
+def print_tree(root, prev=nil, is_right=false)
+  return if root.nil?
+
+  prev_str = "    "
+  trunk = Trunk.new prev, prev_str
+  print_tree root.right, trunk, true
+
+  if prev.nil?
+    trunk.str = "———"
+  elsif is_right
+    trunk.str = "/———"
+    prev_str = "   |"
+  else
+    trunk.str = "\\———"
+    prev.str = prev_str
+  end
+
+  show_trunk trunk
+  puts " #{root.val}"
+  prev.str = prev_str if prev
+  trunk.str = "   |"
+  print_tree root.left, trunk, false
+end
