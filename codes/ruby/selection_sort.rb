80509c1b50a74fb0246fdee0d123a5319ff9f6c2
cy-by-side <153972766+cy-by-side@users.noreply.github.com>
2024-04-04 22:08:25 +0800

    Create selection_sort.rb

---
+++ codes/ruby/selection_sort.rb
@@ -0,0 +1,28 @@
+=begin
+File: selection_sort.rb
+Created Time: 2024-04-02
+Author: Cy (9738314@gmail.com)
+=end
+
+### 选择排序 ###
+def selection_sort(nums)
+  n = nums.length()
+  # 外排序：未排序区间为[i, n-1]
+  for i in 0...n - 1
+    k = i
+    # 内循环：找到未排序区间内的最小元素
+    for j in i + 1...n
+      if nums[j] < nums[k]
+        k = j # 记录最小元素的索引
+      end
+    end
+    # 将该最小元素与未排序区间的首个元素交换
+    nums[i], nums[k] = nums[k], nums[i]
+  end
+end
+
+### Driver Code ###
+
+nums = [4,1,3,1,5,2]
+selection_sort(nums)
+puts "选择排序完成后 nums = #{nums}"
