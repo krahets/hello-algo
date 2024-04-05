a263b839b9ef7dc4f3c49865c35d258fbfad90d5
krahets <krahets@163.com>
2024-04-06 03:02:26 +0800

    deploy

---
+++ zh-hant/javascripts/katex.js
@@ -0,0 +1,10 @@
+document$.subscribe(({ body }) => {
+  renderMathInElement(body, {
+    delimiters: [
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false },
+      { left: "\\(", right: "\\)", display: false },
+      { left: "\\[", right: "\\]", display: true },
+    ],
+  });
+});
