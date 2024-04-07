ec0380149bd28d7e70a3fb974672b9853b1f6eea
cy-by-side <153972766+cy-by-side@users.noreply.github.com>
2024-04-07 21:34:22 +0800

    Update bubble_sort.rb

--- codes/ruby/chapter_sorting/bubble_sort.rb
+++ codes/ruby/chapter_sorting/bubble_sort.rb
@@ -1,14 +1,14 @@
 =begin
 File: bubble_sort.rb
 Created Time: 2024-04-02
-Author: Cy (9738314@gmail.com)
+Author: Cy (9738314@gmail.com) Xuan Khoa Tu Nguyen (ngxktuzkai2000@gmail.com)
 =end
 
 ### 冒泡排序 ###
 def bubble_sort(nums)
-  n = nums.length()
+  n = nums.length
   # 外循环：未排序区间为 [0, i]
-  for i in (n - 1...0).step(-1)
+  for i in (n - 1).downto(1)
     # 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
     for j in 0...i
       if nums[j] > nums[j + 1]
@@ -23,7 +23,8 @@ end
 def bubble_sort_with_flag(nums)
   n = nums.length()
   # 外循环：未排序区间为 [0, i]
-  for i in (n - 1...0).step(-1)
+  for i in (n - 1).downto(1)
+    # 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
     flag = false  # 初始化标志位
     # 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
     for j in 0...i
@@ -33,18 +34,16 @@ def bubble_sort_with_flag(nums)
         flag = true  # 记录交换元素
       end
     end
-    if flag == false
-      return
-    end
+    break unless false
   end
 end
 
 ### Driver Code ###
 
-nums = [8,5,6,4,3,7,10,2,9,4]
+nums = [8, 5, 6, 4, 3, 7, 10, 2, 9, 4]
 bubble_sort(nums)
 puts "冒泡排序完成后 nums = #{nums}"
 
-nums1 = [8,5,6,4,3,7,10,2,9,4]
+nums1 = [8, 5, 6, 4, 3, 7, 10, 2, 9, 4]
 bubble_sort_with_flag(nums1)
 puts "冒泡排序完成后 nums1 = #{nums1}"
