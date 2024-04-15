a33f959dfe0ac71b9ca2e0f33578f9d7f559aeea
krahets <krahets@163.com>
2024-04-15 19:35:59 +0800

    Fix is_empty() implementation in the stack and queue chapter

--- codes/python/chapter_stack_and_queue/linkedlist_deque.py
+++ codes/python/chapter_stack_and_queue/linkedlist_deque.py
@@ -30,7 +30,7 @@ class LinkedListDeque:
 
     def is_empty(self) -> bool:
         """判断双向队列是否为空"""
-        return self.size() == 0
+        return self._size == 0
 
     def push(self, num: int, is_front: bool):
         """入队操作"""
