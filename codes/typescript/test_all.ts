7e46f6d4a7745edb471f4599dd6f230d98d14201
Shyam-Chen <shyamchen1994@gmail.com>
2024-04-16 13:51:28 +0800

    feat(TypeScript): Add the workflow file for CI check

---
+++ codes/typescript/test_all.ts
@@ -0,0 +1,39 @@
+import { expandGlob, type WalkEntry } from 'jsr:@std/fs';
+
+const entries = [] as WalkEntry[];
+
+for await (const entry of expandGlob('./chapter_*/*.ts')) {
+    entries.push(entry);
+}
+
+const errors = [] as ReadableStream<Uint8Array>[];
+
+for (const file of entries) {
+    const command = new Deno.Command('tsx', {
+        args: [file.path],
+        cwd: import.meta.dirname,
+        stdin: 'piped',
+        stdout: 'piped',
+        stderr: 'piped',
+    });
+
+    const process = command.spawn();
+    const { success } = await process.status;
+    if (!success) errors.push(process.stderr);
+}
+
+console.log(`Tested ${entries.length} files`);
+console.log(`Found exception in ${errors.length} files`);
+
+if (errors.length) {
+    console.log();
+
+    for (const error of errors) {
+        const reader = error.getReader();
+        const { value } = await reader.read();
+        const decoder = new TextDecoder();
+        console.error(decoder.decode(value));
+    }
+
+    throw new Error('Test failed');
+}
