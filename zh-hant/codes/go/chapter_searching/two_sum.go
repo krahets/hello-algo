6a4b324673a4ec6dfedc38304f4846fc6f93ccd0
krahets <krahets@163.com>
2024-04-22 02:33:45 +0800

    Sync zh and zh-hant versions

--- zh-hant/codes/go/chapter_searching/two_sum.go
+++ zh-hant/codes/go/chapter_searching/two_sum.go
@@ -9,7 +9,7 @@ func twoSumBruteForce(nums []int, target int) []int {
 	size := len(nums)
 	// 兩層迴圈，時間複雜度為 O(n^2)
 	for i := 0; i < size-1; i++ {
-		for j := i + 1; i < size; j++ {
+		for j := i + 1; j < size; j++ {
 			if nums[i]+nums[j] == target {
 				return []int{i, j}
 			}
