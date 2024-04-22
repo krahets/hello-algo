aa998449dc38106c3b65bda0436cef3e59abf271
bluebean-cloud <1396211327@qq.com>
2024-04-22 15:46:03 +0800

    init heap Ruby

---
+++ codes/ruby/chapter_heap/heap.rb
@@ -0,0 +1,5 @@
+=begin
+File: heap.rb
+Created Time: 2024-04-19
+Author: Blue Bean (lonnnnnnner@gmail.com)
+=end
---
+++ codes/ruby/chapter_heap/top_k.rb
@@ -0,0 +1,5 @@
+=begin
+File: top_k.rb
+Created Time: 2024-04-19
+Author: Blue Bean (lonnnnnnner@gmail.com)
+=end
