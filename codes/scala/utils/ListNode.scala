83158bcdd8f645242af54eedccb76896c62cdb58
WangBlue <2712256194@qq.com>
2024-08-10 17:42:51 +0800

    链表的节点操作

--- codes/scala/utils/ListNode.scala
+++ codes/scala/utils/ListNode.scala
@@ -8,9 +8,10 @@
 /* 链表节点 */
 
 class ListNode {
-  var value: Int = 0
-  var next: ListNode = _
+  var value: Int = 0        // 节点值
+  var next: ListNode = _   // 指向下一个节点的指针
 
+  /* 构造函数 */
   def this(value: Int) = {
     this()
     this.value = value
