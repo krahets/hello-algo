eafa2284ecfa8bf5a1325fd6730bbc949782448d
chen yunliang <676814828@qq.com>
2025-07-07 11:20:16 +0800

    Update array.py
    insert 方法需要先给列表扩展一个位置，否则最后一个元素会丢失

--- codes/python/chapter_array_and_linkedlist/array.py
+++ codes/python/chapter_array_and_linkedlist/array.py
@@ -31,6 +31,8 @@ def extend(nums: list[int], enlarge: int) -> list[int]:
 
 def insert(nums: list[int], num: int, index: int):
     """在数组的索引 index 处插入元素 num"""
+    # 需要先给列表扩展一个位置，再执行右移
+    nums.append(0)
     # 把索引 index 以及之后的所有元素向后移动一位
     for i in range(len(nums) - 1, index, -1):
         nums[i] = nums[i - 1]
