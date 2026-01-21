e9e3156e1f97b84b1f65222f9c7140822e78ca4e
Lingxuan Ye <ink@lingxuan.io>
2026-01-22 00:45:53 +0800

    refactor: simplify implementation

--- codes/rust/src/linked_list.rs
+++ codes/rust/src/linked_list.rs
@@ -1,12 +1,8 @@
 use std::cell::RefCell;
 use std::collections::HashMap;
+use std::fmt;
 use std::hash::Hash;
 use std::rc::Rc;
-
-#[derive(Debug)]
-pub struct LinkedList<T> {
-    pub head: Option<ListNode<T>>,
-}
 
 #[derive(Debug)]
 pub struct ListNode<T> {
@@ -16,104 +12,89 @@ pub struct ListNode<T> {
 
 pub type ListLink<T> = Rc<RefCell<ListNode<T>>>;
 
-impl<T> LinkedList<T> {
-    fn from_iter<I>(mut iter: I) -> Self
+impl<T> ListNode<T> {
+    pub fn new(val: T) -> ListLink<T> {
+        let node = Self { val, next: None };
+        ListLink::from(node)
+    }
+
+    pub fn from_slice(slice: &[T]) -> Option<ListLink<T>>
+    where
+        T: Clone,
+    {
+        let iter = slice.iter().cloned();
+        Self::from_de_iter(iter)
+    }
+
+    pub fn from_array<const N: usize>(array: [T; N]) -> Option<ListLink<T>> {
+        let iter = array.into_iter();
+        Self::from_de_iter(iter)
+    }
+
+    pub fn from_vec(vec: Vec<T>) -> Option<ListLink<T>> {
+        let iter = vec.into_iter();
+        Self::from_de_iter(iter)
+    }
+
+    fn from_de_iter<I>(mut iter: I) -> Option<ListLink<T>>
     where
         I: DoubleEndedIterator<Item = T>,
     {
-        let Some(head) = iter.next() else {
-            return Self { head: None };
-        };
+        let head = iter.next()?;
 
         let mut next = None;
         for val in iter.rev() {
-            let node = ListNode { val, next };
+            let node = Self { val, next };
             next = Some(ListLink::from(node));
         }
 
-        Self {
-            head: Some(ListNode { val: head, next }),
-        }
-    }
-}
-
-impl<T> From<&[T]> for LinkedList<T>
-where
-    T: Clone,
-{
-    fn from(value: &[T]) -> Self {
-        let iter = value.iter().cloned();
-        Self::from_iter(iter)
-    }
-}
-
-impl<T, const N: usize> From<[T; N]> for LinkedList<T> {
-    fn from(value: [T; N]) -> Self {
-        let iter = value.into_iter();
-        Self::from_iter(iter)
-    }
-}
-
-impl<T> From<Vec<T>> for LinkedList<T> {
-    fn from(value: Vec<T>) -> Self {
-        let iter = value.into_iter();
-        Self::from_iter(iter)
-    }
-}
-
-impl<T> From<LinkedList<T>> for HashMap<T, ListLink<T>>
-where
-    T: Clone + Eq + Hash,
-{
-    fn from(value: LinkedList<T>) -> Self {
-        let mut map = HashMap::new();
-        let Some(head) = value.head else {
-            return map;
-        };
-
-        let val = head.val.clone();
-        let mut current = match &head.next {
-            None => {
-                map.insert(val, ListLink::from(head));
-                return map;
-            }
-            Some(next) => {
-                let next = Rc::clone(next);
-                map.insert(val, ListLink::from(head));
-                next
-            }
-        };
-
-        loop {
-            let borrow = current.borrow();
-            let val = borrow.val.clone();
-            match &borrow.next {
-                None => {
-                    drop(borrow);
-                    map.insert(val, current);
-                    break;
-                }
-                Some(next) => {
-                    let next = Rc::clone(next);
-                    drop(borrow);
-                    map.insert(val, current);
-                    current = next;
-                }
-            };
-        }
-
-        map
-    }
-}
-
-impl<T> ListNode<T> {
-    pub fn new(val: T) -> Self {
-        Self { val, next: None }
+        let head = Self { val: head, next };
+        Some(ListLink::from(head))
     }
 }
 
 impl<T> From<ListNode<T>> for ListLink<T> {
     fn from(value: ListNode<T>) -> Self {
         Rc::new(RefCell::new(value))
+    }
+}
+
+impl<T> From<ListNode<T>> for HashMap<T, ListLink<T>>
+where
+    T: Clone + Eq + Hash,
+{
+    fn from(value: ListNode<T>) -> Self {
+        let mut map = HashMap::new();
+
+        let val = value.val.clone();
+        let mut next = value.next.clone();
+        let link = ListLink::from(value);
+        map.insert(val, link);
+
+        while let Some(link) = next {
+            let borrow = link.borrow();
+            let val = borrow.val.clone();
+            next = borrow.next.clone();
+            drop(borrow);
+            map.insert(val, link);
+        }
+
+        map
+    }
+}
+
+impl<T> fmt::Display for ListNode<T>
+where
+    T: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.val)?;
+        let mut next = self.next.clone();
+        while let Some(link) = next {
+            let borrow = link.borrow();
+            write!(f, " -> {}", borrow.val)?;
+            next = borrow.next.clone();
+        }
+        Ok(())
     }
 }
