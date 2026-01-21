94300bb3910aa8c7c222eb66697d8fc884c99e0d
Lingxuan Ye <ink@lingxuan.io>
2026-01-22 05:06:31 +0800

    refactor: improve readability

--- codes/rust/chapter_hashing/array_hash_map.rs
+++ codes/rust/chapter_hashing/array_hash_map.rs
@@ -10,13 +10,15 @@ pub struct Pair {
     pub key: i32,
     pub val: String,
 }
+
 /* 基于数组实现的哈希表 */
 pub struct ArrayHashMap {
     buckets: Vec<Option<Pair>>,
 }
 
 impl ArrayHashMap {
-    pub fn new() -> ArrayHashMap {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
         // 初始化数组，包含 100 个桶
         Self {
             buckets: vec![None; 100],
@@ -29,18 +31,15 @@ impl ArrayHashMap {
     }
 
     /* 查询操作 */
-    pub fn get(&self, key: i32) -> Option<&String> {
+    pub fn get(&self, key: i32) -> Option<&str> {
         let index = self.hash_func(key);
-        self.buckets[index].as_ref().map(|pair| &pair.val)
+        self.buckets[index].as_ref().map(|pair| &pair.val[..])
     }
 
     /* 添加操作 */
-    pub fn put(&mut self, key: i32, val: &str) {
+    pub fn put(&mut self, key: i32, val: String) {
         let index = self.hash_func(key);
-        self.buckets[index] = Some(Pair {
-            key,
-            val: val.to_string(),
-        });
+        self.buckets[index] = Some(Pair { key, val });
     }
 
     /* 删除操作 */
@@ -67,10 +66,10 @@ impl ArrayHashMap {
     }
 
     /* 获取所有值 */
-    pub fn value_set(&self) -> Vec<&String> {
+    pub fn value_set(&self) -> Vec<&str> {
         self.buckets
             .iter()
-            .filter_map(|pair| pair.as_ref().map(|pair| &pair.val))
+            .filter_map(|pair| pair.as_ref().map(|pair| &pair.val[..]))
             .collect()
     }
 
@@ -87,18 +86,18 @@ fn main() {
     let mut map = ArrayHashMap::new();
     /*添加操作 */
     // 在哈希表中添加键值对（key, value）
-    map.put(12836, "小哈");
-    map.put(15937, "小啰");
-    map.put(16750, "小算");
-    map.put(13276, "小法");
-    map.put(10583, "小鸭");
+    map.put(12836, "小哈".to_string());
+    map.put(15937, "小啰".to_string());
+    map.put(16750, "小算".to_string());
+    map.put(13276, "小法".to_string());
+    map.put(10583, "小鸭".to_string());
     println!("\n添加完成后，哈希表为\nKey -> Value");
     map.print();
 
     /* 查询操作 */
     // 向哈希表中输入键 key ，得到值 value
     let name = map.get(15937).unwrap();
-    println!("\n输入学号 15937 ，查询到姓名 {}", name);
+    println!("\n输入学号 15937 ，查询到姓名 {name}");
 
     /* 删除操作 */
     // 在哈希表中删除键值对 (key, value)
@@ -107,18 +106,18 @@ fn main() {
     map.print();
 
     /* 遍历哈希表 */
-    println!("\n遍历键值对 Key->Value");
+    println!("\n遍历键值对 Key -> Value");
     for pair in map.entry_set() {
         println!("{} -> {}", pair.key, pair.val);
     }
 
     println!("\n单独遍历键 Key");
     for key in map.key_set() {
-        println!("{}", key);
+        println!("{key}");
     }
 
     println!("\n单独遍历值 Value");
     for val in map.value_set() {
-        println!("{}", val);
+        println!("{val}");
     }
 }
