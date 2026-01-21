53b1f03cceb60cf553c434f7c78f9bb762dba69f
Lingxuan Ye <ink@lingxuan.io>
2026-01-22 04:35:31 +0800

    refactor: improve readability

--- codes/rust/chapter_stack_and_queue/queue.rs
+++ codes/rust/chapter_stack_and_queue/queue.rs
@@ -4,8 +4,6 @@
  * Author: codingonion (coderonion@gmail.com), xBLACKICEx (xBLACKICEx@outlook.com)
  */
 
-use hello_algo_rust::include::print_util;
-
 use std::collections::VecDeque;
 
 /* Driver Code */
@@ -19,23 +17,21 @@ pub fn main() {
     queue.push_back(2);
     queue.push_back(5);
     queue.push_back(4);
-    print!("队列 queue = ");
-    print_util::print_queue(&queue);
+    println!("队列 queue = {queue:?}");
 
     // 访问队首元素
     let peek = queue.front().unwrap();
-    println!("\n队首元素 peek = {peek}");
+    println!("队首元素 peek = {peek}");
 
     // 元素出队
     let pop = queue.pop_front().unwrap();
-    print!("出队元素 pop = {pop}，出队后 queue = ");
-    print_util::print_queue(&queue);
+    println!("出队元素 pop = {pop}，出队后 queue = {queue:?}");
 
     // 获取队列的长度
     let size = queue.len();
-    print!("\n队列长度 size = {size}");
+    println!("队列长度 size = {size}");
 
     // 判断队列是否为空
     let is_empty = queue.is_empty();
-    print!("\n队列是否为空 = {is_empty}");
+    println!("队列是否为空 = {is_empty}");
 }
