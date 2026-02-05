99fcfed8a347672922fb91852c3bc932cca4fb0d
Lingxuan Ye <ink@lingxuan.io>
2026-02-05 21:06:03 +0800

    refactor: replace field `extend_ratio` with associated constant `EXTEND_RATIO`

--- codes/rust/chapter_array_and_linkedlist/my_list.rs
+++ codes/rust/chapter_array_and_linkedlist/my_list.rs
@@ -8,20 +8,20 @@ use hello_algo_rust::include::print_util;
 
 /* 列表类 */
 pub struct MyList {
-    arr: Vec<i32>,       // 数组（存储列表元素）
-    capacity: usize,     // 列表容量
-    size: usize,         // 列表长度（当前元素数量）
-    extend_ratio: usize, // 每次列表扩容的倍数
+    arr: Vec<i32>,   // 数组（存储列表元素）
+    capacity: usize, // 列表容量
+    size: usize,     // 列表长度（当前元素数量）
 }
 
 impl MyList {
+    const EXTEND_RATIO: usize = 2; // 每次列表扩容的倍数
+
     /* 构造方法 */
     pub fn new(capacity: usize) -> Self {
         Self {
             arr: vec![0; capacity],
             capacity,
             size: 0,
-            extend_ratio: 2,
         }
     }
 
@@ -105,7 +105,7 @@ impl MyList {
             // 先前实现没有考虑初始容量为 0 的情况，这会导致容量无法增长
             4
         } else {
-            self.capacity * self.extend_ratio
+            self.capacity * Self::EXTEND_RATIO
         };
         self.arr.resize(new_capacity, 0);
         // 更新列表容量
