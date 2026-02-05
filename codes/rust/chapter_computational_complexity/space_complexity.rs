9080fb48efb93866974ff377c3e1529be202893c
Lingxuan Ye <ink@lingxuan.io>
2026-02-05 20:30:09 +0800

    refactor: simplify code

--- codes/rust/chapter_computational_complexity/space_complexity.rs
+++ codes/rust/chapter_computational_complexity/space_complexity.rs
@@ -7,10 +7,13 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use hello_algo_rust::include::{ListNode, TreeNode, print_util};
+use hello_algo_rust::binary_tree::BinaryTree;
+use hello_algo_rust::linked_list::ListNode;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
+
+type TreeNode = hello_algo_rust::binary_tree::TreeNode<i32>;
 
 /* 函数 */
 fn function() -> i32 {
@@ -40,20 +43,16 @@ fn linear(n: usize) {
     // 长度为 n 的数组占用 O(n) 空间
     let nums = vec![0; n];
     // 长度为 n 的列表占用 O(n) 空间
-    let mut nodes = Vec::new();
-    for i in 0..n {
-        nodes.push(ListNode::new(i))
-    }
+    let nodes = (0..n).map(ListNode::new).collect::<Vec<_>>();
     // 长度为 n 的哈希表占用 O(n) 空间
-    let mut map = HashMap::new();
-    for i in 0..n {
-        map.insert(i, i.to_string());
-    }
+    let map = (0..n)
+        .map(|i| (i, i.to_string()))
+        .collect::<HashMap<_, _>>();
 }
 
 /* 线性阶（递归实现） */
 fn linear_recur(n: usize) {
-    println!("递归 n = {}", n);
+    println!("递归 n = {n}");
     if n == 1 {
         return;
     }
@@ -62,27 +61,18 @@ fn linear_recur(n: usize) {
 
 /* 平方阶 */
 fn quadratic(n: usize) {
-    // 矩阵占用 O(n^2) 空间
-    let num_matrix = vec![vec![0; n]; n];
     // 二维列表占用 O(n^2) 空间
-    let mut num_list = Vec::new();
-    for _ in 0..n {
-        let mut tmp = Vec::new();
-        for _ in 0..n {
-            tmp.push(0);
-        }
-        num_list.push(tmp);
-    }
+    let num_matrix = vec![vec![0; n]; n];
 }
 
 /* 平方阶（递归实现） */
 fn quadratic_recur(n: usize) -> i32 {
     if n == 0 {
         return 0;
-    };
+    }
     // 数组 nums 长度为 n, n-1, ..., 2, 1
     let nums = vec![0; n];
-    println!("递归 n = {} 中的 nums 长度 = {}", n, nums.len());
+    println!("递归 n = {n} 中的 nums 长度 = {n}");
     quadratic_recur(n - 1)
 }
 
@@ -90,7 +80,7 @@ fn quadratic_recur(n: usize) -> i32 {
 fn build_tree(n: usize) -> Option<Rc<RefCell<TreeNode>>> {
     if n == 0 {
         return None;
-    };
+    }
     let root = TreeNode::new(0);
     root.borrow_mut().left = build_tree(n - 1);
     root.borrow_mut().right = build_tree(n - 1);
@@ -110,5 +100,5 @@ fn main() {
     quadratic_recur(n);
     // 指数阶
     let root = build_tree(n);
-    print_util::print_tree(&root.unwrap());
+    println!("{}", root.display())
 }
