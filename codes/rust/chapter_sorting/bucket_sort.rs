5db98b318d529e9b9a34b8c3600ab9d15e6ebf9e
Lingxuan Ye <ink@lingxuan.io>
2026-02-06 00:50:34 +0800

    refactor: use `f64::total_cmp`for float sorting to follow best practices

--- codes/rust/chapter_sorting/bucket_sort.rs
+++ codes/rust/chapter_sorting/bucket_sort.rs
@@ -4,11 +4,12 @@
  * Author: night-cruise (2586447362@qq.com)
  */
 
-use hello_algo_rust::include::print_util;
-
 /* 桶排序 */
 pub fn bucket_sort(nums: &mut [f64]) {
-    // 初始化 k = n/2 个桶，预期向每个桶分配 2 个元素
+    // 为了提高健壮性，应遍历并断言所有输入均在 [0, 1) 范围内，
+    // 此处省略该步骤，假设输入均符合要求
+
+    // 初始化 k = n / 2 个桶，预期向每个桶分配 2 个元素
     let k = nums.len() / 2;
     let mut buckets = vec![vec![]; k];
     // 1. 将数组元素分配到各个桶中
@@ -21,7 +22,7 @@ pub fn bucket_sort(nums: &mut [f64]) {
     // 2. 对各个桶执行排序
     for bucket in &mut buckets {
         // 使用内置排序函数，也可以替换成其他排序算法
-        bucket.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        bucket.sort_by(f64::total_cmp);
     }
     // 3. 遍历桶合并结果
     let mut i = 0;
@@ -38,6 +39,5 @@ fn main() {
     // 设输入数据为浮点数，范围为 [0, 1)
     let mut nums = [0.49, 0.96, 0.82, 0.09, 0.57, 0.43, 0.91, 0.75, 0.15, 0.37];
     bucket_sort(&mut nums);
-    print!("桶排序完成后 nums = ");
-    print_util::print_array(&nums);
+    println!("桶排序完成后 nums = {nums:?}");
 }
