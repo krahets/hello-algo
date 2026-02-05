961ca9696a3f5cc6e27031ff4d01b9933e091e86
Lingxuan Ye <ink@lingxuan.io>
2026-02-06 01:42:00 +0800

    refactor: simplify code and improve consistency

--- codes/rust/chapter_backtracking/preorder_traversal_ii_compact.rs
+++ codes/rust/chapter_backtracking/preorder_traversal_ii_compact.rs
@@ -4,49 +4,47 @@
  * Author: codingonion (coderonion@gmail.com)
  */
 
-use hello_algo_rust::include::{print_util, vec_to_tree, TreeNode};
-use std::{cell::RefCell, rc::Rc};
+use hello_algo_rust::binary_tree::{BinaryTree, TreeLink};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub type TreeNode = hello_algo_rust::binary_tree::TreeNode<i32>;
 
 /* 前序遍历：例题二 */
 pub fn pre_order(
     res: &mut Vec<Vec<Rc<RefCell<TreeNode>>>>,
     path: &mut Vec<Rc<RefCell<TreeNode>>>,
-    root: Option<&Rc<RefCell<TreeNode>>>,
+    root: &Option<Rc<RefCell<TreeNode>>>,
 ) {
-    if root.is_none() {
-        return;
+    let Some(node) = root else { return };
+    // 尝试
+    path.push(node.clone());
+    if node.borrow().val == 7 {
+        // 记录解
+        res.push(path.clone());
     }
-    if let Some(node) = root {
-        // 尝试
-        path.push(node.clone());
-        if node.borrow().val == 7 {
-            // 记录解
-            res.push(path.clone());
-        }
-        pre_order(res, path, node.borrow().left.as_ref());
-        pre_order(res, path, node.borrow().right.as_ref());
-        // 回退
-        path.pop();
-    }
+    pre_order(res, path, &node.borrow().left);
+    pre_order(res, path, &node.borrow().right);
+    // 回退
+    path.pop();
 }
 
 /* Driver Code */
 fn main() {
-    let root = vec_to_tree([1, 7, 3, 4, 5, 6, 7].map(|x| Some(x)).to_vec());
-    println!("初始化二叉树");
-    print_util::print_tree(root.as_ref().unwrap());
+    let root = TreeLink::try_from_array([1, 7, 3, 4, 5, 6, 7].map(Some)).ok();
+    println!("初始化二叉树\n{}", root.display());
 
     // 前序遍历
     let mut path = Vec::new();
     let mut res = Vec::new();
-    pre_order(&mut res, &mut path, root.as_ref());
+    pre_order(&mut res, &mut path, &root);
 
-    println!("\n输出所有根节点到节点 7 的路径");
+    println!("输出所有根节点到节点 7 的路径");
     for path in res {
-        let mut vals = Vec::new();
-        for node in path {
-            vals.push(node.borrow().val)
-        }
-        println!("{:?}", vals);
+        let vals = path
+            .iter()
+            .map(|node| node.borrow().val)
+            .collect::<Vec<_>>();
+        println!("{vals:?}");
     }
 }
