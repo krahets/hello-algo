9c44cdbcc98e90fe6615736ceb16120819f86ad1
Lingxuan Ye <ink@lingxuan.io>
2026-02-06 00:38:37 +0800

    refactor: adjust comments and simplify formatted output logic

--- codes/rust/chapter_sorting/bubble_sort.rs
+++ codes/rust/chapter_sorting/bubble_sort.rs
@@ -4,8 +4,6 @@
  * Author: codingonion (coderonion@gmail.com)
  */
 
-use hello_algo_rust::include::print_util;
-
 /* 冒泡排序 */
 pub fn bubble_sort(nums: &mut [i32]) {
     // 外循环：未排序区间为 [0, i]
@@ -24,18 +22,21 @@ pub fn bubble_sort(nums: &mut [i32]) {
 pub fn bubble_sort_with_flag(nums: &mut [i32]) {
     // 外循环：未排序区间为 [0, i]
     for i in (1..nums.len()).rev() {
-        let mut flag = false; // 初始化标志位
+        // 初始化标志位
+        let mut flag = false;
         // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
         for j in 0..i {
             if nums[j] > nums[j + 1] {
                 // 交换 nums[j] 与 nums[j + 1]
                 nums.swap(j, j + 1);
-                flag = true; // 记录交换元素
+                // 记录交换元素
+                flag = true;
             }
         }
         if !flag {
-            break; // 此轮“冒泡”未交换任何元素，直接跳出
-        };
+            // 此轮“冒泡”未交换任何元素，直接跳出
+            break;
+        }
     }
 }
 
@@ -43,11 +44,9 @@ pub fn bubble_sort_with_flag(nums: &mut [i32]) {
 fn main() {
     let mut nums = [4, 1, 3, 1, 5, 2];
     bubble_sort(&mut nums);
-    print!("冒泡排序完成后 nums = ");
-    print_util::print_array(&nums);
+    println!("冒泡排序完成后 nums = {nums:?}");
 
-    let mut nums1 = [4, 1, 3, 1, 5, 2];
-    bubble_sort_with_flag(&mut nums1);
-    print!("\n冒泡排序完成后 nums1 = ");
-    print_util::print_array(&nums1);
+    let mut nums = [4, 1, 3, 1, 5, 2];
+    bubble_sort_with_flag(&mut nums);
+    println!("冒泡排序完成后 nums = {nums:?}");
 }
--- codes/rust/chapter_sorting/selection_sort.rs
+++ codes/rust/chapter_sorting/selection_sort.rs
@@ -4,8 +4,6 @@
  * Author: WSL0809 (wslzzy@outlook.com)
  */
 
-use hello_algo_rust::include::print_util;
-
 /* 选择排序 */
 pub fn selection_sort(nums: &mut [i32]) {
     if nums.is_empty() {
@@ -13,12 +11,13 @@ pub fn selection_sort(nums: &mut [i32]) {
     }
     let n = nums.len();
     // 外循环：未排序区间为 [i, n-1]
-    for i in 0..n - 1 {
+    for i in 0..(n - 1) {
         // 内循环：找到未排序区间内的最小元素
         let mut k = i;
-        for j in i + 1..n {
+        for j in (i + 1)..n {
             if nums[j] < nums[k] {
-                k = j; // 记录最小元素的索引
+                // 记录最小元素的索引
+                k = j;
             }
         }
         // 将该最小元素与未排序区间的首个元素交换
@@ -30,6 +29,5 @@ pub fn selection_sort(nums: &mut [i32]) {
 fn main() {
     let mut nums = [4, 1, 3, 1, 5, 2];
     selection_sort(&mut nums);
-    print!("\n选择排序完成后 nums = ");
-    print_util::print_array(&nums);
+    println!("选择排序完成后 nums = {nums:?}");
 }
