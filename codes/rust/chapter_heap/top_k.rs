6abcb05a9b7407e4e09548a9cdfca8b2f1bd3d9f
Lingxuan Ye <ink@lingxuan.io>
2026-02-06 00:02:10 +0800

    fix: prevent `top_k_heap` from panicking when `k` is zero

--- codes/rust/chapter_heap/top_k.rs
+++ codes/rust/chapter_heap/top_k.rs
@@ -4,8 +4,7 @@
  * Author: night-cruise (2586447362@qq.com)
  */
 
-use hello_algo_rust::include::print_util;
-
+use hello_algo_rust::heap::Heap;
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 
@@ -13,6 +12,10 @@ use std::collections::BinaryHeap;
 pub fn top_k_heap(nums: Vec<i32>, k: usize) -> BinaryHeap<Reverse<i32>> {
     // BinaryHeap 是大顶堆，使用 Reverse 将元素取反，从而实现小顶堆
     let mut heap = BinaryHeap::<Reverse<i32>>::new();
+    // 确保之后的代码路径不会操作空堆
+    if nums.is_empty() || k == 0 {
+        return heap;
+    }
     // 将数组的前 k 个元素入堆
     for &num in nums.iter().take(k) {
         heap.push(Reverse(num));
@@ -20,7 +23,11 @@ pub fn top_k_heap(nums: Vec<i32>, k: usize) -> BinaryHeap<Reverse<i32>> {
     // 从第 k+1 个元素开始，保持堆的长度为 k
     for &num in nums.iter().skip(k) {
         // 若当前元素大于堆顶元素，则将堆顶元素出堆、当前元素入堆
-        if num > heap.peek().unwrap().0 {
+        let Some(&Reverse(peek)) = heap.peek() else {
+            // 空堆已提前返回，此处不可达
+            unreachable!()
+        };
+        if num > peek {
             heap.pop();
             heap.push(Reverse(num));
         }
@@ -33,7 +40,11 @@ fn main() {
     let nums = vec![1, 7, 6, 3, 2];
     let k = 3;
 
-    let res = top_k_heap(nums, k);
-    println!("最大的 {} 个元素为", k);
-    print_util::print_heap(res.into_iter().map(|item| item.0).collect());
+    let res = top_k_heap(nums, k)
+        .iter()
+        .map(|val| val.0) // Reverse 没有实现 std::fmt::Display
+        .collect::<Vec<_>>();
+    println!("最大的 {k} 个元素为");
+    println!("数组表示：{}", res.display_as_array());
+    println!("树状表示：\n{}", res.display_as_tree());
 }
