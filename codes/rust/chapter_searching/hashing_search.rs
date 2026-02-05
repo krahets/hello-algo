4f05b1776a15b953538ba96271d48d0e54de74b6
Lingxuan Ye <ink@lingxuan.io>
2026-02-06 00:30:55 +0800

    fix: resolve warning
    Removed:
    - `clippy::needless_lifetimes`

--- codes/rust/chapter_searching/hashing_search.rs
+++ codes/rust/chapter_searching/hashing_search.rs
@@ -4,15 +4,15 @@
  * Author: codingonion (coderonion@gmail.com)
  */
 
-use hello_algo_rust::include::ListNode;
+use hello_algo_rust::linked_list::{LinkedList, ListNode};
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
 
 /* 哈希查找（数组） */
-pub fn hashing_search_array<'a>(map: &'a HashMap<i32, usize>, target: i32) -> Option<&'a usize> {
+pub fn hashing_search_array(map: &HashMap<i32, usize>, target: i32) -> Option<&usize> {
     // 哈希表的 key: 目标元素，value: 索引
-    // 若哈希表中无此 key ，返回 None
+    // 若哈希表中无此 key，返回 None
     map.get(&target)
 }
 
@@ -22,7 +22,7 @@ pub fn hashing_search_linked_list(
     target: i32,
 ) -> Option<&Rc<RefCell<ListNode<i32>>>> {
     // 哈希表的 key: 目标节点值，value: 节点对象
-    // 若哈希表中无此 key ，返回 None
+    // 若哈希表中无此 key，返回 None
     map.get(&target)
 }
 
@@ -34,17 +34,16 @@ fn main() {
     let nums = [1, 5, 3, 2, 4, 7, 5, 9, 10, 8];
     // 初始化哈希表
     let mut map = HashMap::new();
-    for (i, num) in nums.iter().enumerate() {
-        map.insert(*num, i); // key: 元素，value: 索引
+    for (i, &num) in nums.iter().enumerate() {
+        map.insert(num, i); // key: 元素，value: 索引
     }
-    let index = hashing_search_array(&map, target);
-    println!("目标元素 3 的索引 = {}", index.unwrap());
+    let index = hashing_search_array(&map, target).unwrap();
+    println!("目标元素 3 的索引 = {index}");
 
     /* 哈希查找（链表） */
-    let head = ListNode::arr_to_linked_list(&nums);
+    let head = ListNode::try_from_array(nums).unwrap();
     // 初始化哈希表
-    // let mut map1 = HashMap::new();
-    let map1 = ListNode::linked_list_to_hashmap(head);
+    let map1 = HashMap::from(head);
     let node = hashing_search_linked_list(&map1, target);
-    println!("目标节点值 3 的对应节点对象为 {:?}", node);
+    println!("目标节点值 3 的对应节点对象为 {node:?}");
 }
