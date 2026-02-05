368166270396266e9a4c79dc1cc8113f4797d9d6
Lingxuan Ye <ink@lingxuan.io>
2026-02-05 21:06:20 +0800

    fix: resolve warning
    Explicitly omitted:
    - `clippy::vec_init_then_push`

--- codes/rust/chapter_stack_and_queue/stack.rs
+++ codes/rust/chapter_stack_and_queue/stack.rs
@@ -4,9 +4,8 @@
  * Author: codingonion (coderonion@gmail.com)
  */
 
-use hello_algo_rust::include::print_util;
-
 /* Driver Code */
+#[allow(clippy::vec_init_then_push)]
 fn main() {
     // 初始化栈
     // 在 rust 中，推荐将 Vec 当作栈来使用
@@ -18,23 +17,21 @@ fn main() {
     stack.push(2);
     stack.push(5);
     stack.push(4);
-    print!("栈 stack = ");
-    print_util::print_array(&stack);
+    println!("栈 stack = {stack:?}");
 
     // 访问栈顶元素
     let peek = stack.last().unwrap();
-    print!("\n栈顶元素 peek = {peek}");
+    println!("栈顶元素 peek = {peek}");
 
     // 元素出栈
     let pop = stack.pop().unwrap();
-    print!("\n出栈元素 pop = {pop}，出栈后 stack = ");
-    print_util::print_array(&stack);
+    println!("出栈元素 pop = {pop}，出栈后 stack = {stack:?}");
 
     // 获取栈的长度
     let size = stack.len();
-    print!("\n栈的长度 size = {size}");
+    println!("栈的长度 size = {size}");
 
     // 判断栈是否为空
     let is_empty = stack.is_empty();
-    print!("\n栈是否为空 = {is_empty}");
+    println!("栈是否为空 = {is_empty}");
 }
