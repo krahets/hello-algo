cb80ebee0ee2deb4325f4397356780358f0aa995
Lingxuan Ye <ink@lingxuan.io>
2026-02-05 20:28:30 +0800

    fix: resolve warnings
    Removed:
    - `clippy::needless_return`
    
    Explicitly omitted:
    - `dead_code`
    - `unused_variables`

--- codes/rust/chapter_computational_complexity/space_complexity.rs
+++ codes/rust/chapter_computational_complexity/space_complexity.rs
@@ -4,7 +4,10 @@
  * Author: codingonion (coderonion@gmail.com)
  */
 
-use hello_algo_rust::include::{print_util, ListNode, TreeNode};
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use hello_algo_rust::include::{ListNode, TreeNode, print_util};
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
@@ -12,11 +15,10 @@ use std::rc::Rc;
 /* 函数 */
 fn function() -> i32 {
     // 执行某些操作
-    return 0;
+    0
 }
 
 /* 常数阶 */
-#[allow(unused)]
 fn constant(n: i32) {
     // 常量、变量、对象占用 O(1) 空间
     const A: i32 = 0;
@@ -24,20 +26,19 @@ fn constant(n: i32) {
     let nums = vec![0; 10000];
     let node = ListNode::new(0);
     // 循环中的变量占用 O(1) 空间
-    for i in 0..n {
+    for _ in 0..n {
         let c = 0;
     }
     // 循环中的函数占用 O(1) 空间
-    for i in 0..n {
+    for _ in 0..n {
         function();
     }
 }
 
 /* 线性阶 */
-#[allow(unused)]
 fn linear(n: i32) {
     // 长度为 n 的数组占用 O(n) 空间
-    let mut nums = vec![0; n as usize];
+    let nums = vec![0; n as usize];
     // 长度为 n 的列表占用 O(n) 空间
     let mut nodes = Vec::new();
     for i in 0..n {
@@ -60,15 +61,14 @@ fn linear_recur(n: i32) {
 }
 
 /* 平方阶 */
-#[allow(unused)]
 fn quadratic(n: i32) {
     // 矩阵占用 O(n^2) 空间
     let num_matrix = vec![vec![0; n as usize]; n as usize];
     // 二维列表占用 O(n^2) 空间
     let mut num_list = Vec::new();
-    for i in 0..n {
+    for _ in 0..n {
         let mut tmp = Vec::new();
-        for j in 0..n {
+        for _ in 0..n {
             tmp.push(0);
         }
         num_list.push(tmp);
@@ -83,7 +83,7 @@ fn quadratic_recur(n: i32) -> i32 {
     // 数组 nums 长度为 n, n-1, ..., 2, 1
     let nums = vec![0; n as usize];
     println!("递归 n = {} 中的 nums 长度 = {}", n, nums.len());
-    return quadratic_recur(n - 1);
+    quadratic_recur(n - 1)
 }
 
 /* 指数阶（建立满二叉树） */
@@ -94,7 +94,7 @@ fn build_tree(n: i32) -> Option<Rc<RefCell<TreeNode>>> {
     let root = TreeNode::new(0);
     root.borrow_mut().left = build_tree(n - 1);
     root.borrow_mut().right = build_tree(n - 1);
-    return Some(root);
+    Some(root)
 }
 
 /* Driver Code */
