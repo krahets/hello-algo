16289f14c31ae9565fada40f5d38d52ab06c7517
Lingxuan Ye <ink@lingxuan.io>
2026-02-06 00:29:26 +0800

    refactor: decouple binary crates and replace all return types with `Option<usize>`
    Refer to codes/rust/GUIDELINES.md for more information.

--- codes/rust/chapter_searching/binary_search_edge.rs
+++ codes/rust/chapter_searching/binary_search_edge.rs
@@ -4,45 +4,62 @@
  * Author: night-cruise (2586447362@qq.com)
  */
 
+use std::cmp::Ordering;
+
 /* 二分查找最左一个 target */
-pub fn binary_search_left_edge(nums: &[i32], target: i32) -> i32 {
+pub fn binary_search_left_edge(nums: &[i32], target: i32) -> Option<usize> {
     // 等价于查找 target 的插入点
     let i = binary_search_insertion(nums, target);
-    // 未找到 target ，返回 -1
-    if i == nums.len() as i32 || nums[i as usize] != target {
-        return -1;
+    // 未找到 target ，返回 None
+    if i == nums.len() || nums[i] != target {
+        return None;
     }
     // 找到 target ，返回索引 i
-    i
+    Some(i)
 }
 
 /* 二分查找最右一个 target */
-pub fn binary_search_right_edge(nums: &[i32], target: i32) -> i32 {
+pub fn binary_search_right_edge(nums: &[i32], target: i32) -> Option<usize> {
     // 转化为查找最左一个 target + 1
-    let i = binary_search_insertion(nums, target + 1);
-    // j 指向最右一个 target ，i 指向首个大于 target 的元素
+    let Some(next) = target.checked_add(1) else {
+        return nums.len().checked_sub(1);
+    };
+    let i = binary_search_insertion(nums, next);
+    // i 指向首个大于 target 的元素，如果 i == 0 ，说明数组中不存在 target
+    if i == 0 {
+        return None;
+    }
+    // j 指向最右一个 target
     let j = i - 1;
-    // 未找到 target ，返回 -1
-    if j == -1 || nums[j as usize] != target {
-        return -1;
+    // 未找到 target ，返回 None
+    if nums[j] != target {
+        return None;
     }
     // 找到 target ，返回索引 j
-    j
+    Some(j)
 }
 
 // 在之前的代码里，这个函数导入自 binary_search_insertion.rs 文件。
 // 然而，binary_search_insertion.rs 是一个 bin crate，不应被当作
 // 模块使用，这是一种不良实践。为了方便起见，我们将该函数直接复制过来。
-fn binary_search_insertion(nums: &[i32], target: i32) -> i32 {
-    let (mut i, mut j) = (0, nums.len() as i32 - 1); // 初始化双闭区间 [0, n-1]
+fn binary_search_insertion(nums: &[i32], target: i32) -> usize {
+    if nums.is_empty() {
+        return 0;
+    }
+    let mut i = 0;
+    let mut j = nums.len() - 1;
     while i <= j {
         let m = i + (j - i) / 2;
-        if nums[m as usize] < target {
-            i = m + 1;
-        } else if nums[m as usize] > target {
-            j = m - 1;
-        } else {
-            j = m - 1;
+        match target.cmp(&nums[m]) {
+            Ordering::Less => match m.checked_sub(1) {
+                None => return 0,
+                Some(v) => j = v,
+            },
+            Ordering::Greater => i = m + 1,
+            Ordering::Equal => match m.checked_sub(1) {
+                None => return 0,
+                Some(v) => j = v,
+            },
         }
     }
     i
@@ -52,13 +69,13 @@ fn binary_search_insertion(nums: &[i32], target: i32) -> i32 {
 fn main() {
     // 包含重复元素的数组
     let nums = [1, 3, 6, 6, 6, 6, 6, 10, 12, 15];
-    println!("\n数组 nums = {:?}", nums);
+    println!("数组 nums = {nums:?}");
 
     // 二分查找左边界和右边界
     for target in [6, 7] {
         let index = binary_search_left_edge(&nums, target);
-        println!("最左一个元素 {} 的索引为 {}", target, index);
+        println!("最左一个元素 {target} 的索引为 {index:?}");
         let index = binary_search_right_edge(&nums, target);
-        println!("最右一个元素 {} 的索引为 {}", target, index);
+        println!("最右一个元素 {target} 的索引为 {index:?}");
     }
 }
--- codes/rust/chapter_searching/binary_search_insertion.rs
+++ codes/rust/chapter_searching/binary_search_insertion.rs
@@ -3,19 +3,34 @@
  * Created Time: 2023-08-30
  * Author: night-cruise (2586447362@qq.com)
  */
-#![allow(unused)]
+
+use std::cmp::Ordering;
 
 /* 二分查找插入点（无重复元素） */
-pub fn binary_search_insertion_simple(nums: &[i32], target: i32) -> i32 {
-    let (mut i, mut j) = (0, nums.len() as i32 - 1); // 初始化双闭区间 [0, n-1]
+pub fn binary_search_insertion_simple(nums: &[i32], target: i32) -> usize {
+    // 数组为空时，返回索引位置 0 作为插入点
+    if nums.is_empty() {
+        return 0;
+    }
+    // 初始化双闭区间 [0, n - 1]
+    let mut i = 0;
+    // 数组不为空，不会发生减法溢出
+    let mut j = nums.len() - 1;
     while i <= j {
-        let m = i + (j - i) / 2; // 计算中点索引 m
-        if nums[m as usize] < target {
-            i = m + 1; // target 在区间 [m+1, j] 中
-        } else if nums[m as usize] > target {
-            j = m - 1; // target 在区间 [i, m-1] 中
-        } else {
-            return m;
+        // 计算中点索引 m
+        let m = i + (j - i) / 2;
+        match target.cmp(&nums[m]) {
+            // target 在区间 [i, m - 1] 中
+            Ordering::Less => match m.checked_sub(1) {
+                // 如果 m.checked_sub(1) 返回 None，说明 m == 0，
+                // 所以 target < nums[0]，返回索引位置 0 作为插入点
+                None => return 0,
+                Some(v) => j = v,
+            },
+            // target 在区间 [m + 1, j] 中
+            Ordering::Greater => i = m + 1,
+            // target 在索引 m 处，返回插入点 m
+            Ordering::Equal => return m,
         }
     }
     // 未找到 target ，返回插入点 i
@@ -23,16 +38,35 @@ pub fn binary_search_insertion_simple(nums: &[i32], target: i32) -> i32 {
 }
 
 /* 二分查找插入点（存在重复元素） */
-pub fn binary_search_insertion(nums: &[i32], target: i32) -> i32 {
-    let (mut i, mut j) = (0, nums.len() as i32 - 1); // 初始化双闭区间 [0, n-1]
+pub fn binary_search_insertion(nums: &[i32], target: i32) -> usize {
+    // 数组为空时，返回索引位置 0 作为插入点
+    if nums.is_empty() {
+        return 0;
+    }
+    // 初始化双闭区间 [0, n - 1]
+    let mut i = 0;
+    // 数组不为空，不会发生减法溢出
+    let mut j = nums.len() - 1;
     while i <= j {
-        let m = i + (j - i) / 2; // 计算中点索引 m
-        if nums[m as usize] < target {
-            i = m + 1; // target 在区间 [m+1, j] 中
-        } else if nums[m as usize] > target {
-            j = m - 1; // target 在区间 [i, m-1] 中
-        } else {
-            j = m - 1; // 首个小于 target 的元素在区间 [i, m-1] 中
+        // 计算中点索引 m
+        let m = i + (j - i) / 2;
+        match target.cmp(&nums[m]) {
+            // target 在区间 [i, m - 1] 中
+            Ordering::Less => match m.checked_sub(1) {
+                // 如果 m.checked_sub(1) 返回 None，说明 m == 0，
+                // 所以 target < nums[0]，返回索引位置 0 作为插入点
+                None => return 0,
+                Some(v) => j = v,
+            },
+            // target 在区间 [m + 1, j] 中
+            Ordering::Greater => i = m + 1,
+            // 索引 m 处存在 target，继续向左查找索引最小的 target
+            Ordering::Equal => match m.checked_sub(1) {
+                // 如果 m.checked_sub(1) 返回 None，说明 m == 0，
+                // 所以最左 target 在数组起点，返回索引位置 0 作为插入点
+                None => return 0,
+                Some(v) => j = v,
+            },
         }
     }
     // 返回插入点 i
@@ -43,19 +77,19 @@ pub fn binary_search_insertion(nums: &[i32], target: i32) -> i32 {
 fn main() {
     // 无重复元素的数组
     let nums = [1, 3, 6, 8, 12, 15, 23, 26, 31, 35];
-    println!("\n数组 nums = {:?}", nums);
+    println!("数组 nums = {nums:?}");
     // 二分查找插入点
     for target in [6, 9] {
         let index = binary_search_insertion_simple(&nums, target);
-        println!("元素 {} 的插入点的索引为 {}", target, index);
+        println!("元素 {target} 的插入点的索引为 {index}");
     }
 
     // 包含重复元素的数组
     let nums = [1, 3, 6, 6, 6, 6, 6, 10, 12, 15];
-    println!("\n数组 nums = {:?}", nums);
+    println!("数组 nums = {nums:?}");
     // 二分查找插入点
     for target in [2, 6, 20] {
         let index = binary_search_insertion(&nums, target);
-        println!("元素 {} 的插入点的索引为 {}", target, index);
+        println!("元素 {target} 的插入点的索引为 {index}");
     }
 }
