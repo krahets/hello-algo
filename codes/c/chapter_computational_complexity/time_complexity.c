26a0e81749dbbf85f49c819e2c45ea15e385ef57
mazezen <mazezen24@gmail.com>
2026-05-18 16:59:35 +0800

    fix: issue_1907, function constant, In C language, there is an unnecessary variable named 'int i'

--- codes/c/chapter_computational_complexity/time_complexity.c
+++ codes/c/chapter_computational_complexity/time_complexity.c
@@ -7,41 +7,49 @@
 #include "../utils/common.h"
 
 /* 常数阶 */
-int constant(int n) {
+int constant(int n)
+{
     int count = 0;
     int size = 100000;
-    int i = 0;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         count++;
     }
     return count;
 }
 
 /* 线性阶 */
-int linear(int n) {
+int linear(int n)
+{
     int count = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         count++;
     }
     return count;
 }
 
 /* 线性阶（遍历数组） */
-int arrayTraversal(int *nums, int n) {
+int arrayTraversal(int *nums, int n)
+{
     int count = 0;
     // 循环次数与数组长度成正比
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         count++;
     }
     return count;
 }
 
 /* 平方阶 */
-int quadratic(int n) {
+int quadratic(int n)
+{
     int count = 0;
     // 循环次数与数据大小 n 成平方关系
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             count++;
         }
     }
@@ -49,13 +57,17 @@ int quadratic(int n) {
 }
 
 /* 平方阶（冒泡排序） */
-int bubbleSort(int *nums, int n) {
+int bubbleSort(int *nums, int n)
+{
     int count = 0; // 计数器
     // 外循环：未排序区间为 [0, i]
-    for (int i = n - 1; i > 0; i--) {
+    for (int i = n - 1; i > 0; i--)
+    {
         // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
-        for (int j = 0; j < i; j++) {
-            if (nums[j] > nums[j + 1]) {
+        for (int j = 0; j < i; j++)
+        {
+            if (nums[j] > nums[j + 1])
+            {
                 // 交换 nums[j] 与 nums[j + 1]
                 int tmp = nums[j];
                 nums[j] = nums[j + 1];
@@ -68,12 +80,15 @@ int bubbleSort(int *nums, int n) {
 }
 
 /* 指数阶（循环实现） */
-int exponential(int n) {
+int exponential(int n)
+{
     int count = 0;
     int bas = 1;
     // 细胞每轮一分为二，形成数列 1, 2, 4, 8, ..., 2^(n-1)
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < bas; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < bas; j++)
+        {
             count++;
         }
         bas *= 2;
@@ -83,16 +98,19 @@ int exponential(int n) {
 }
 
 /* 指数阶（递归实现） */
-int expRecur(int n) {
+int expRecur(int n)
+{
     if (n == 1)
         return 1;
     return expRecur(n - 1) + expRecur(n - 1) + 1;
 }
 
 /* 对数阶（循环实现） */
-int logarithmic(int n) {
+int logarithmic(int n)
+{
     int count = 0;
-    while (n > 1) {
+    while (n > 1)
+    {
         n = n / 2;
         count++;
     }
@@ -100,36 +118,42 @@ int logarithmic(int n) {
 }
 
 /* 对数阶（递归实现） */
-int logRecur(int n) {
+int logRecur(int n)
+{
     if (n <= 1)
         return 0;
     return logRecur(n / 2) + 1;
 }
 
 /* 线性对数阶 */
-int linearLogRecur(int n) {
+int linearLogRecur(int n)
+{
     if (n <= 1)
         return 1;
     int count = linearLogRecur(n / 2) + linearLogRecur(n / 2);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         count++;
     }
     return count;
 }
 
 /* 阶乘阶（递归实现） */
-int factorialRecur(int n) {
+int factorialRecur(int n)
+{
     if (n == 0)
         return 1;
     int count = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         count += factorialRecur(n - 1);
     }
     return count;
 }
 
 /* Driver Code */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // 可以修改 n 运行，体会一下各种复杂度的操作数量变化趋势
     int n = 8;
     printf("输入数据大小 n = %d\n", n);
@@ -146,7 +170,8 @@ int main(int argc, char *argv[]) {
 
     count = quadratic(n);
     printf("平方阶的操作数量 = %d\n", count);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         nums[i] = n - i; // [n,n-1,...,2,1]
     }
     count = bubbleSort(nums, n);
@@ -169,7 +194,8 @@ int main(int argc, char *argv[]) {
     printf("阶乘阶（递归实现）的操作数量 = %d\n", count);
 
     // 释放堆区内存
-    if (nums != NULL) {
+    if (nums != NULL)
+    {
         free(nums);
         nums = NULL;
     }
